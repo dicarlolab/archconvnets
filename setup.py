fee48b9240af05d14120a035014c0cdb70733d5c
Diego Ardila <ardila@mit.edu>
2013-10-10 18:02:45 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -161,6 +161,8 @@ if os.path.exists(requirements_file):
     print 'DEFINIITTTEEEEELY FOUND THE FILE'
     if requirements is None:
         requirements = parse_requirements(requirements_file)
+        print requirements
+        print '----------------------------------------'
     if dependency_links is None:
         dependency_links = parse_dependency_links(requirements_file)
 else:
