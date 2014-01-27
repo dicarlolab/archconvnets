d8cf9146c77ab765001258b5483f2f849b1f8f75
Diego Ardila <ardila@mit.edu>
2014-01-27 13:48:04 -0500

    Update synthetic_training.py

--- examples/synthetic_training.py
+++ examples/synthetic_training.py
@@ -23,4 +23,12 @@ if __name__ == "main":
 """
 Use this command to run (see Alex's documenatation for explanation of these parameters)
 python run_synthetic_training.py --data-path=/export/imgnet_storage_full/yamins_skdata/sythetic_batches_0 --crop=7 --save-path=/export/imgnet_storage_full/ --test-range=950-999 --train-range=0-949 --layer-def=/home/yamins/archconvnets/archconvnets/convnet/ut_model_full/layer_nofc_0.cfg --layer-params=/home/darren/archconvnets/archconvnets/convnet/ut_model_full/layer-params.cfg --data-provider=general-cropped --test-freq=50 --conserve-mem=1 --max-filesize=99999999 --img-size=128
+
+To store, with experiment_id="mytestrun"
+
+python run_synthetic_training.py --data-path=/export/imgnet_storage_full/yamins_skdata/sythetic_batches_0 --crop=7 --save-path=/export/imgnet_storage_full/ --test-range=950-999 --train-range=0-50 --layer-def=/home/yamins/archconvnets/archconvnets/convnet/ut_model_full/layer_nofc_0.cfg --layer-params=/home/darren/archconvnets/archconvnets/convnet/ut_model_full/layer-params.cfg --data-provider=general-cropped --test-freq=10 --conserve-mem=1 --max-filesize=99999999 --img-size=128 --save-db=1 --experiment-id=mytestrun
+
+To load for further running:
+
+python run_synthetic_training.py --data-path=/export/imgnet_storage_full/yamins_skdata/sythetic_batches_0 --save-path=/export/imgnet_storage_full/ --test-range=950-999 --train-range=0-50 --layer-params=/home/darren/archconvnets/archconvnets/convnet/ut_model_full/layer-params.cfg --data-provider=general-cropped --test-freq=10 --conserve-mem=1 --max-filesize=99999999 --img-size=128 --save-db=1 --load-query='{"experiment_id":"mytestrun"}'
 """
