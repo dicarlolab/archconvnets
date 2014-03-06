1bdc1099453dcf7f2c10967a0157d2600dde9e38
Dan Yamins <dyamins@gmail.com>
2014-03-06 00:18:01 -0500

    cifar params update

--- archconvnets/hyperopt/cifar_params_new.py
+++ archconvnets/hyperopt/cifar_params_new.py
@@ -1,3 +1,4 @@
+import copy
 from collections import OrderedDict
 import numpy as np
 
@@ -11,11 +12,11 @@ from hyperopt.pyll_utils import hp_normal, hp_lognormal, hp_qnormal, hp_qlognorm
 from hyperopt.pyll_utils import hp_choice
 
 
-conv_template = [OrderedDict([('type', 'conv'),
+conv_template = lambda i : [OrderedDict([('type', 'conv'),
                                     ('filters', scope.int(hp_qloguniform('conv_num_filters_%d' % i, np.log(16), np.log(96), q=16))),
                                     ('padding',  hp_choice('conv_padding_%d' % i, [1, 2])),
                                     ('stride', hp_choice('conv_stride_%d' % i, [1, 2])),
-                                    ('filtersize', scope.int(hp_quniform('conv_filter_shape_%d' % i, 2, 12, 1))),
+                                    ('filterSize', scope.int(hp_quniform('conv_filter_shape_%d' % i, 2, 8, 1))),
                                     ('neuron', 'relu'),
                                     ('initw', hp_choice('conv_initw_%d' % i, [1e-4, 1e-3, 1e-2, 1e-1])),
                                     ('partialsum', 1),
@@ -26,29 +27,29 @@ conv_template = [OrderedDict([('type', 'conv'),
                                ('momb', hp_uniform('conv_momb_%d' % i, .55, .95)),
                                ('wc', 0)])]
 
-pool_template = [OrderedDict([('type', 'pool'),
+pool_template = lambda i : [OrderedDict([('type', 'pool'),
                                     ('pool', hp_choice('pool_type_%d' % i, 
                                                         ['max', 
                                                          'avg',
                                                          scope.int(hp_quniform('pool_order_%d' % i, 2, 12, q=2))])),
                                     ('start', 0),
-                                    ('sizex', scope.int(hp_quniform('pool_sizex_%d' % i, 2, 5, 1))),
+                                    ('sizeX', scope.int(hp_quniform('pool_sizex_%d' % i, 2, 5, 1))),
                                     ('stride', hp_choice('pool_stride_%d' % i, [1, 2])),
-                                    ('outputsx', 0)]),
+                                    ('outputsX', 0)]),
                 None]
 
 
-norm_template = [OrderedDict([('type', 'cmrnorm'),
+norm_template = lambda i: [OrderedDict([('type', 'cmrnorm'),
                               ('size', scope.int(hp_quniform('rnorm_size_%d' % i, 5, 12, 1)))]), 
                  OrderedDict([('scale', hp_choice('norm_scale_%d' % i, [1e-4, 1e-3, 1e-2])),
                               ('pow', hp_uniform('norm_pow_%d' % i, .55, .95))])]
 
 
-local_template = [OrderedDict([('type', 'local'),
+local_template = lambda i : [OrderedDict([('type', 'local'),
                                      ('filters', scope.int(hp_qloguniform('local_num_filters_%d' % i, np.log(16), np.log(96), q=16))),
                                      ('padding', hp_choice('clocal_padding_%d' % i, [1, 2])),
                                      ('stride', hp_choice('local_stride_%d' % i, [1, 2])),
-                                     ('filtersize', scope.int(hp_quniform('local_filter_shape_%d' % i, 2, 12, 1))),
+                                     ('filterSize', scope.int(hp_quniform('local_filter_shape_%d' % i, 2, 12, 1))),
                                      ('neuron', 'relu'),
                                      ('initw', hp_choice('local_initw_%d' % i, [1e-4, 1e-3, 1e-2, 1e-1]))]), 
                   OrderedDict([('epsw', hp_choice('local_epsw_%d' % i, [1e-3, 1e-2, 1e-1])),
@@ -58,21 +59,20 @@ local_template = [OrderedDict([('type', 'local'),
                                ('wc', 0.004)])]
 
 def channel_layers(N):
-    return [hp_choice('channel_node_%d' % i, [conv_template , 
-                                                pool_template,
-                                                norm_template,
-                                                local_template]) 
+    return [hp_choice('channel_node_%d' % i, [conv_template(i), 
+                                                pool_template(i),
+                                                norm_template(i),
+                                                local_template(i)]) 
                                             for i in range(N)]
 
 def channel_layers_nolocal(N):
-    return [hp_choice('channel_node_%d' % i, [conv_template , 
-                                                pool_template,
-                                                norm_template]) 
+    return [hp_choice('channel_node_%d' % i, [conv_template(i), 
+                                                pool_template(i),
+                                                norm_template(i)]) 
                                             for i in range(N)]
 
 final_layers = OrderedDict([('fc1', OrderedDict([('type', 'fcdropo'),
                                     ('outputs', scope.int(hp_qloguniform('fc1_num_outputs', np.log(64), np.log(256), q=64))),
-                                    ('inputs', 'channel_node_9'),
                                     ('initw', hp_choice('fc1_initw', [1e-3, 1e-2, 1e-1])),
                                     ('neuron', 'relu'),
                                     ('rate', 0.5)])),
@@ -86,15 +86,15 @@ final_layers = OrderedDict([('fc1', OrderedDict([('type', 'fcdropo'),
                                       ('inputs', 'labels,probs')]))])
                                       
 
-final_layers_learning_params = OrderedDict([('fc128', OrderedDict([('epsw', hp_choice('fc1_epsw_%d' % i, [1e-3, 1e-2, 1e-1])),
-                                           ('epsb', hp_choice('fc1_epsb_%d' % i, [2e-4, 2e-3, 2e-2])),
-                                           ('momw', hp_uniform('fc1_momw_%d' % i, .55, .95)),
-                                           ('momb', hp_uniform('fc1_momb_%d' % i, .55, .95)),
+final_layers_learning_params = OrderedDict([('fc1', OrderedDict([('epsw', hp_choice('fc1_epsw', [1e-3, 1e-2, 1e-1])),
+                                           ('epsb', hp_choice('fc1_epsb', [2e-4, 2e-3, 2e-2])),
+                                           ('momw', hp_uniform('fc1_momw', .55, .95)),
+                                           ('momb', hp_uniform('fc1_momb', .55, .95)),
                                            ('wc', 0.004)])),
-                                            ('fc10', OrderedDict([('epsw', hp_choice('fc10_epsw_%d' % i, [1e-3, 1e-2, 1e-1])),
-                                           ('epsb', hp_choice('fc10_epsb_%d' % i, [2e-4, 2e-3, 2e-2])),
-                                           ('momw', hp_uniform('fc10_momw_%d' % i, .55, .95)),
-                                           ('momb', hp_uniform('fc10_momb_%d' % i, .55, .95)),
+                                            ('fc10', OrderedDict([('epsw', hp_choice('fc10_epsw', [1e-3, 1e-2, 1e-1])),
+                                           ('epsb', hp_choice('fc10_epsb', [2e-4, 2e-3, 2e-2])),
+                                           ('momw', hp_uniform('fc10_momw', .55, .95)),
+                                           ('momb', hp_uniform('fc10_momb', .55, .95)),
                                            ('wc', 0.01)])),
                                            ('logprob', OrderedDict([('coeff', '1')]))])
                                            
@@ -115,15 +115,16 @@ def config_interpretation(config):
     n1 = 'channel_layer_0_%s' % config['channel_layers'][0][0]['type']
     layers[n1] = config['channel_layers'][0][0]
     layers[n1]['channels'] = 3
-    layers[n1]['input'] = 'data'
-    params[n1] = config['channel_layers'][0][1]
+    layers[n1]['inputs'] = 'data'
+    if config['channel_layers'][0][1] is not None:
+        params[n1] = config['channel_layers'][0][1]
     
     i = 1
     for cn, cnl in config['channel_layers'][1:]:
         n1 = 'channel_layer_%d_%s' % (i, cn['type'])
         n0 = 'channel_layer_%d_%s' % (i-1, config['channel_layers'][i-1][0]['type'])
         layers[n1] = cn
-        layers[n1]['input'] = n0
+        layers[n1]['inputs'] = n0
         if layers[n0]['type'] in ['local', 'conv']:
             layers[n1]['channels'] = layers[n0]['filters']
         else:
@@ -131,19 +132,37 @@ def config_interpretation(config):
         
         if cnl is not None:
             params[n1] = cnl
-            
-    
         
         i += 1
-        
-    layers.update(config['final_layers'])
+
+    final_layers = copy.deepcopy(config['final_layers'])
+    final_layers['fc1']['inputs'] = n1
+
+    layers.update(final_layers)
     params.update(config['final_params'])
-    
-    return layers, params
-    
-                  
-    
-                            
 
+    newlayers = OrderedDict([])
+    for (l_ind, l) in enumerate(layers):            
+        if layers[l]['type'] == 'pool' and isinstance(layers[l]['pool'], int):
+            order = layers[l]['pool']
+            layers[l]['pool'] = 'avg'
+            l0 = OrderedDict([('type', 'neuron'),
+                              ('neuron', 'power[%.3f]' % order),
+                              ('inputs', layers[l]['inputs'])])
+            l0name = l + '_pre'
+            layers[l]['inputs'] = l0name
+            l1 = OrderedDict([('type', 'neuron'),
+                              ('neuron', 'power[%.3f]' % (1./order)),
+                              ('inputs', l)
+                              ])
+            l1name = l + '_post'
+            if l_ind < len(layers):
+                layers[layers.keys()[l_ind + 1]]['inputs'] = l1name
+            newlayers[l0name] = l0
+            newlayers[l] = layers[l]
+            newlayers[l1name] = l1
+        else:
+            newlayers[l] = layers[l]
 
-                          
+    return {'layer_def': newlayers,
+            'learning_params': params}
--- archconvnets/hyperopt/cifar_prediction.py
+++ archconvnets/hyperopt/cifar_prediction.py
@@ -17,6 +17,7 @@ from ..convnet.api import odict_to_config
 from ..convnet.layer import LayerParsingError
 
 from . import cifar_params
+from . import cifar_params_new
 from .hyperopt_helpers import suggest_multiple_from_name
 
 
@@ -26,6 +27,16 @@ def cifar_random_experiment0(experiment_id):
     port = 22334
     bandit = 'cifar_prediction_bandit'
     bandit_kwargdict = {'param_args': {}, 'experiment_id': experiment_id}
+    exp = cifar_random_experiment(dbname, host, port, bandit, bandit_kwargdict)
+    return exp
+
+
+def cifar_random_experiment_new(experiment_id):
+    dbname = 'cifar_predictions_random_experiment_new'
+    host = 'localhost'
+    port = 22334
+    bandit = 'cifar_prediction_bandit_new'
+    bandit_kwargdict = {'param_args': {'num_layers': 10}, 'experiment_id': experiment_id}
     exp = cifar_random_experiment(dbname, host, port, bandit, bandit_kwargdict)
     return exp
 
@@ -69,6 +80,18 @@ bandit_exceptions = [
 def cifar_prediction_bandit(argdict):
     template = cifar_params.template_func(argdict['param_args'])
     return scope.cifar_prediction_bandit_evaluate(template, argdict)
+
+
+@hyperopt.base.as_bandit(exceptions=bandit_exceptions)
+def cifar_prediction_bandit_new(argdict):
+    template = cifar_params_new.template_func(argdict['param_args'])
+    interpreted_template = scope.config_interpret(template)
+    return scope.cifar_prediction_bandit_evaluate(interpreted_template, argdict)
+
+
+@scope.define
+def config_interpret(template):
+    return cifar_params_new.config_interpretation(template)
 
 
 @scope.define
