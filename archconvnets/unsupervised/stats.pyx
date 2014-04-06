b582c53cdc7e1153bbebbc5c530304736e54b710
Dan Yamins <dyamins@gmail.com>
2014-04-06 17:16:20 -0400

    flat version of stats

--- archconvnets/unsupervised/stats.pyx
+++ archconvnets/unsupervised/stats.pyx
@@ -25,3 +25,34 @@ def threestats(np.ndarray[np.float64_t, ndim=2] X):
                  B[k, j, i] = B[i, j, k]
                  
     return B / s
+    
+
+def threestats_flat(np.ndarray[np.float64_t, ndim=2] X):
+
+    cdef int s0 = X.shape[0]
+    cdef int s1 = X.shape[1]
+    
+
+    cdef Py_ssize_t i, j, k, ind 
+    
+    cdef int s = (s0**3 + 3*s0**2 + 2*s0)/6
+    
+    ind = 0
+    cdef np.ndarray S = np.zeros([s], np.float64)
+    for i in range(s0):
+        for j in range(i, s0):
+            for k in range(j, s0):
+                S[ind] = X[i].std() * X[j].std() * X[k].std()
+                ind = ind + 1
+
+    X = X - X.mean(1)[:, np.newaxis]
+    
+    ind = 0
+    cdef np.ndarray B = np.zeros([s], np.float64)
+    for i in range(s0):
+        for j in range(i, s0):
+            for k in range(j, s0):
+                 B[ind] = (X[i] * X[j] * X[k]).mean()
+                 ind = ind + 1
+                 
+    return B / S
