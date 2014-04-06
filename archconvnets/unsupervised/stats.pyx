bd902b29be8f9b30c711dd2c136bf92de66b3cf2
Dan Yamins <dyamins@gmail.com>
2014-04-06 17:30:01 -0400

    flat version of 3rd order stats

--- archconvnets/unsupervised/stats.pyx
+++ archconvnets/unsupervised/stats.pyx
@@ -28,25 +28,21 @@ def threestats(np.ndarray[np.float64_t, ndim=2] X):
     
 
 def threestats_flat(np.ndarray[np.float64_t, ndim=2] X):
-
     cdef int s0 = X.shape[0]
     cdef int s1 = X.shape[1]
-    
-
     cdef Py_ssize_t i, j, k, ind 
-    
     cdef int s = (s0**3 + 3*s0**2 + 2*s0)/6
+    cdef np.ndarray sval = X.std(1)
     
-    ind = 0
+    ind = 0  
     cdef np.ndarray S = np.zeros([s], np.float64)
     for i in range(s0):
         for j in range(i, s0):
             for k in range(j, s0):
-                S[ind] = X[i].std() * X[j].std() * X[k].std()
+                S[ind] = sval[i] * sval[j] * sval[k]
                 ind = ind + 1
 
     X = X - X.mean(1)[:, np.newaxis]
-    
     ind = 0
     cdef np.ndarray B = np.zeros([s], np.float64)
     for i in range(s0):
