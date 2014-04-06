2b0d716b386012ff4435df92a52fc232859af166
= <yamins@mit.edu>
2014-04-06 15:32:00 -0400

    update to cython

--- cython_setup.py
+++ cython_setup.py
@@ -3,6 +3,6 @@ import numpy as np
 from Cython.Build import cythonize
 
 setup(
-    ext_modules = cythonize("stats.pyx"),
+    ext_modules = cythonize("archconvnets/unsupervised/stats.pyx"),
     include_dirs=[np.get_include()]
 )
