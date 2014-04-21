7645cf84b48c830de17689cc3e722c57bddee15b
Darren Seibert <darren@munctional1.mit.edu>
2014-04-21 19:50:25 -0400

    fixed hyperopt saving frequency

--- archconvnets/hyperopt/imgnet_prediction.py
+++ archconvnets/hyperopt/imgnet_prediction.py
@@ -359,7 +359,8 @@ def imgnet_prediction_bandit_evaluate2(config, kwargs, features=None):
 
     op = ConvNet.get_options_parser()
     oppdict = [('--save-db', '1'),
-               ('--save-recent-filters', '0'),
+               ('--save-filters','1'),
+               ('--save-recent-filters', '1'),('--save-recent','1'),
                ('--crop-border', '9'),
                ('--train-range', '0-1250'),#'0-4'),
                ('--test-range', '4351-4550'),#'5'),
@@ -371,7 +372,7 @@ def imgnet_prediction_bandit_evaluate2(config, kwargs, features=None):
                ('--data-path', '/storage/imgnet_256batchsz_138px'),
                ('--dp-params', '{"perm_type": "random", "perm_seed": 0, "preproc": {"normalize": false, "dtype": "float32", "resize_to": [138, 138], "mode": "RGB", "crop": null, "mask": null}, "batch_size": 256, "meta_attribute": "synset", "dataset_name": ["imagenet.dldatasets", "ChallengeSynsets2013_offline"]}'),
                ('--test-freq', kwargs.get('test_freq', 100)),
-               ('--saving-freq', '1'),
+               ('--saving-freq', '0'),
                ('--epochs', kwargs.get('epochs_round0', 5)),
                ('--img-size', '138'),
                ('--experiment-data', exp_str),
