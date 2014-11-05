79aea4ebd4b3bfd42ebb1a2083e9dcc2091a8bef
Darren Seibert <darren@munctional0.(none)>
2014-11-04 19:04:58 -0500

    minor

--- archconvnets/unsupervised/stats_bag.py
+++ archconvnets/unsupervised/stats_bag.py
@@ -1,7 +1,11 @@
 from archconvnets.unsupervised.DFT import DFT_matrix_2d
 from scipy.spatial.distance import pdist
+from scipy.spatial.distance import squareform
+from scipy.io import loadmat
 from archconvnets.unsupervised.grad_transpose import test_grad_transpose
 from archconvnets.unsupervised.grad_fourier_l1 import test_grad_fourier_l1
+from archconvnets.unsupervised.grad_channel_corr import test_grad_channel_corr
+from archconvnets.unsupervised.grad_second_order import test_grad_second_order
 from scipy.io import savemat
 from scipy.stats.mstats import zscore
 import PIL
@@ -26,47 +30,67 @@ def test_F(F):
 			sparsity += np.sum(np.abs(np.dot(F,patch)))
 	l, g =  test_grad_transpose(F.T, 3, 7, n_out)
 	lf, gf = test_grad_fourier_l1(F.T, 3, 7, n_out, X, X2)
-	loss = sse + lambd*sparsity + lambdt*l + lambf*lf
-	print 'recon:',sse, 'sparsity:',sparsity, 'transpose:',lambdt*l, 'fourier:',lambf*lf, 'loss:',loss, 'transpose: ',np.mean(np.abs(1-pdist(F.T,'correlation'))),  img_t
+	lc, gc = test_grad_channel_corr(F.T, 3, 7, n_out)
+	ls, gs = test_grad_second_order(F.T, 3, 7, n_out, c_mat_input)
+	loss = sse + lambds*sparsity + lambdt*l + lambdf*lf + lambdc*lc
+	print 'recon:',sse, 'sparsity:',lambdf*sparsity, 'transpose:',lambdt*l, 'fourier:',lambdf*lf, 'loss:',loss, 'transpose: ',np.mean(np.abs(1-pdist(F.T,'correlation'))),  img_t
+	print 'channel corr:',lambdc*lc, 'second order:',lambds*ls
 	sparsities.append(sparsity)
 	sses.append(sse)
 	transposes.append(l)
 	fouriers.append(lf)
 	losses.append(loss)
+	channel_corrs.append(lc)
+	second_orders.append(ls)
 
 ####### fourier
 X = np.real(DFT_matrix_2d(7))
 X2 = np.imag(DFT_matrix_2d(7))
 
+###### second order
+c_mat_input = np.squeeze(loadmat('/home/darren/pixels_zscore.mat')['f'])
+######
 	
 img_t = 0
 n_out = 32
 n_in = 147
-F = np.random.random((n_out,n_in))
-patch = np.random.random(n_in)
+F = zscore(np.random.random((n_out,n_in)),axis=None)
 
 sparsities = []
 sses = []
 transposes = []
 fouriers = []
 losses = []
+channel_corrs = []
+second_orders = []
 
-lambd = 0#1#e1#e1
-lambdt = 1e4#5
-lambf = 1e1#8
-eps = 5e-3#12
-grad = np.zeros((n_out,n_in))
-n_imgs_batch = 5#128
+lambds = 1e-1#e1#e1    # sparsity
+lambdt = 2.5e1#5        # transpose
+lambdf = 1e-1#8         # fourier
+lambdc = 1
+lambds = 1
+# todo: slowness
+
+eps = 5e-2#3#12
+n_imgs_batch = 3#25#128
+
+data_mean = np.load('/storage/batch128_img138_full/batches.meta')['data_mean'].reshape((3,138,138))[:,:,:,np.newaxis]
+
 for batch in range(9000):
 	t_start = time.time()
 	F = F.reshape((n_out,n_in))
-	batch_data =  np.load('/storage/batch128_img138_full/data_batch_' + str(batch))['data'].reshape((3,138,138,128)).transpose((3,1,2,0))[:,66:66+7,66:66+7]
+	batch_data =  np.load('/storage/batch128_img138_full/data_batch_' + str(batch))['data'].reshape((3,138,138,128)) - data_mean
+	batch_data = batch_data.transpose((3,1,2,0))[:,66:66+7,66:66+7]
 	Ft = pinv(F)
 	one_m_Ft_F = 1 - np.dot(Ft,F)
 	one_m_F_Ft = 1 - np.dot(F,Ft)
 	Ft_Ftt = np.dot(Ft, Ft.T)
+	grad = np.zeros((n_out,n_in))
+	grad_recon = np.zeros((n_out,n_in))
+	grad_sparse = np.zeros((n_out,n_in))
 	
-	for img in range(n_imgs_batch):#128):
+	################# 
+	for img in range(n_imgs_batch):
 		img_t += 1
 
 		patch = batch_data[img].ravel()
@@ -87,22 +111,33 @@ for batch in range(9000):
 				d_recon_dFij = np.dot(dApdp, F_patch) + Ft[:,i_val]*patch[j_val]
 				grad_temp = np.sum(d_recon_dFij*recon_m_patch)
 				if np.isnan(grad_temp) == False:
-					grad[i_val,j_val] += grad_temp
-					grad[i_val,j_val] += lambd*patch[j_val]*np.sign(patch[j_val]*F[i_val,j_val])
+					grad_recon[i_val,j_val] += grad_temp
+					grad_sparse[i_val,j_val] += patch[j_val]*np.sign(patch[j_val]*F[i_val,j_val])
 	l, g =  test_grad_transpose(F.T, 3, 7, n_out)
 	g = g.reshape((n_in, n_out)).T
 	grad_f = lambdt * g
 	
 	lf, gf = test_grad_fourier_l1(F.T, 3, 7, n_out, X, X2)
 	gf = gf.reshape((n_in, n_out)).T
-	grad_f += lambf * gf
-	grad_f += grad
+
+	lc, gc = test_grad_channel_corr(F.T, 3, 7, n_out)
+	gc = gc.reshape((n_in, n_out)).T
+	
+	ls, gs = test_grad_second_order(F.T, 3, 7, n_out, c_mat_input)
+	gs = gs.reshape((n_in, n_out)).T
+	
+	grad_f += lambdf * gf
+	grad_f += lambdc * gc
+	grad_f += lambds * gs
+	grad_f += lambds * grad_sparse
+	grad_f += grad_recon
 	
 	F -= eps*grad_f/n_imgs_batch
 	Ft = pinv(F)
 	test_F(F)
 
-	print 'recon:',eps*np.sum(np.abs(grad)), 'sparsity:',0, 'transpose:',eps*lambdt*np.sum(np.abs(g)), 'fourier:',eps*lambf*np.sum(np.abs(gf)), 'loss:',eps*np.sum(grad+lambdt*g+lambf*gf), 'filters: ', np.sum(np.abs(F))
+	print 'recon:',eps*np.sum(np.abs(grad_recon)), 'sparsity:',eps*np.sum(np.abs(grad_sparse)), 'transpose:',eps*lambdt*np.sum(np.abs(g)), 'fourier:',eps*lambdf*np.sum(np.abs(gf)), 'loss:',eps*np.sum(grad_recon+lambds*grad_sparse+lambdt*g+lambdf*gf), 'filters: ', np.sum(np.abs(F))
+	print 'channel corr:',eps*lambdc*np.sum(np.abs(gc)),'second order:',eps*lambds*np.sum(np.abs(gs))
 	print time.time() - t_start
 
 	filters_cat = np.zeros((3*7,7*n_out))
@@ -111,8 +146,8 @@ for batch in range(9000):
 		filters_cat[0*7:1*7,filter*7:(filter+1)*7] = F[filter,:,:,0]
 		filters_cat[1*7:2*7,filter*7:(filter+1)*7] = F[filter,:,:,1]
 		filters_cat[2*7:3*7,filter*7:(filter+1)*7] = F[filter,:,:,2]
-	savemat('/home/darren/autoencode_transpose.mat',{'F': F, 'filters_cat': filters_cat, 'sparsities':sparsities,'sses':sses, 'transposes': transposes,
-		'fouriers': fouriers, 'losses': losses})
+	savemat('/home/darren/stats_bag_filters.mat',{'F': F, 'filters_cat': filters_cat, 'sparsities':sparsities,'sses':sses, 'transposes': transposes,
+		'fouriers': fouriers, 'losses': losses, 'channel_corrs':channel_corrs,'second_orders':second_orders})
 
 
 
