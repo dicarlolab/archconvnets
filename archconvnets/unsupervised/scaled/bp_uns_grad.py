e5285d1d56a1c7894fce4d4a2eee2f57ea42592b
Darren Seibert <darren@munctional0.(none)>
2014-12-09 16:24:44 -0500

    m

--- archconvnets/unsupervised/scaled/bp_uns_grad.py
+++ archconvnets/unsupervised/scaled/bp_uns_grad.py
@@ -20,7 +20,7 @@ F2_scale = 0.01
 F3_scale = 0.01
 FL_scale = 0.3
 
-EPS = 5e-4#1e-11#e-3#-2#6#7
+EPS = 5e-6#1e-11#e-3#-2#6#7
 #EPS = 1e-3
 eps_F1 = EPS
 eps_F2 = EPS
@@ -34,7 +34,7 @@ N_IMGS = 1 # batch size
 N_TEST_IMGS = 50
 IMG_SZ = 64 # input image size (px)
 
-N = 32
+N = 4
 n1 = N # L1 filters
 n2 = N # ...
 n3 = N
@@ -65,7 +65,7 @@ if False:
 	err = x['err'].tolist()
 	err_test = x['err_test'].tolist()
 else:
-	np.random.seed(663456)
+	np.random.seed(216)
 	F1 = np.random.normal(scale=F1_scale, size=(n1, 3, s1, s1))
 	F1_init = copy.deepcopy(F1)
 	F2 = np.random.normal(scale=F2_scale, size=(n2, n1, s2, s2))
@@ -222,23 +222,23 @@ for step in range(np.int(1e7)):
 	########### F1 deriv wrt f1_, a1_x_, a1_y_, channel_
 	t_grad_start = time.time()
 	
-	a1_x_ = 0
-	a1_y_ = 1
-	channel_ = 1
-	# output_switches3_x.shape = (32,29,29,1)
-	# imgs_1 = (3,5,5,32,29,29,1)
-	pool1_patches = np.random.random((32,29,29,N_IMGS, 3*5*5))
+	# output_switches2_x.shape = (32,11,11,1)
+	pool1_patches = np.zeros((n1, max_output_sz1, max_output_sz1, N_IMGS, 3,s1,s1))
+	for f1 in range(n1):
+		for z1 in range(max_output_sz1):
+			for z2 in range(max_output_sz1):
+				for img in range(N_IMGS):
+					# pool1 -> conv1
+					a1_x_global = output_switches1_x[f1, z1, z2, img]
+					a1_y_global = output_switches1_y[f1, z1, z2, img]
+					# conv1 -> px
+					pool1_patches[f1, z1, z2, img] = imgs_pad[:, a1_x_global:a1_x_global+s1, a1_y_global:a1_y_global+s1, img]
 	
 	grad = np.zeros_like(F1)
 	for a1_x_ in range(s1):
-		print a1_x_
 		for a1_y_ in range(s1):
-			for channel in range(channel_):
-				F1_ident = np.zeros((3,5,5))
-				F1_ident[channel_, a1_x_, a1_y_] = 1
-				F1_ident = F1_ident.ravel()
-				
-				pool1_deriv = np.dot(pool1_patches, F1_ident)
+			for channel_ in range(3):
+				pool1_deriv = pool1_patches[:,:,:,:,channel_,a1_x_,a1_y_]
 				
 				for f1_ in range(n1):
 					conv_output2_deriv = conv_block(F2.transpose((1,2,3,0))[f1_][np.newaxis], pool1_deriv[f1_][np.newaxis])
@@ -249,11 +249,66 @@ for step in range(np.int(1e7)):
 					
 					pred_deriv = np.dot(FL.reshape((N_C, n3*max_output_sz3**2)), max_output3.reshape((n3*max_output_sz3**2, N_IMGS))).T
 					
-					grad[f1_, channel_, a1_x_, a1_y_] = -np.dot(pred_deriv, pred)
+					grad[f1_, channel_, a1_x_, a1_y_] = np.dot(pred_deriv, pred)
+	F1 -= eps_F1 * grad
 	
+	########## F2 deriv wrt f2_, a2_x_, a2_y_, f1_
+	# output_switches2_x.shape = (32,11,11,1)
+	pool2_patches = np.zeros((n2, max_output_sz2, max_output_sz2, N_IMGS, n1,s2,s2))
+	for f2 in range(n2):
+		for z1 in range(max_output_sz2):
+			for z2 in range(max_output_sz2):
+				for img in range(N_IMGS):
+					# pool2 -> conv2
+					a2_x_global = output_switches2_x[f2, z1, z2, img]
+					a2_y_global = output_switches2_y[f2, z1, z2, img]
+					# conv2 -> pool1
+					pool2_patches[f2, z1, z2, img] = max_output1[:, a2_x_global:a2_x_global+s2, a2_y_global:a2_y_global+s2, img]
 	
-	#conv_grad_output1 = conv_block()
+	grad = np.zeros_like(F2)
+	for a2_x_ in range(s2):
+		for a2_y_ in range(s2):
+			for f1_ in range(n1):
+				pool2_deriv = pool2_patches[:,:,:,:,f1_,a2_x_,a2_y_]
+				
+				for f2_ in range(n2):
+					conv_output3_deriv = conv_block(F3.transpose((1,2,3,0))[f2_][np.newaxis], pool2_deriv[f2_][np.newaxis])
+					max_output3, output_switches3_x, output_switches3_y = max_pool_locs_alt(conv_output3_deriv, conv_output3)
+					
+					pred_deriv = np.dot(FL.reshape((N_C, n3*max_output_sz3**2)), max_output3.reshape((n3*max_output_sz3**2, N_IMGS))).T
+					
+					grad[f2_, f1_, a2_x_, a2_y_] = np.dot(pred_deriv, pred)
+	F2 -= eps_F2 * grad
 
+	########## F3 deriv wrt f3_, a3_x_, a3_y_, f2_
+	# output_switches3_x.shape = (n3,3,3,1)
+	# conv_output2.shape = (n2,25,25,1)
+	pool3_patches = np.zeros((n3, max_output_sz3, max_output_sz3, N_IMGS, n2,s3,s3))
+	for f3 in range(n3):
+		for z1 in range(max_output_sz3):
+			for z2 in range(max_output_sz3):
+				for img in range(N_IMGS):
+					# pool3 -> conv3
+					a3_x_global = output_switches3_x[f3, z1, z2, img]
+					a3_y_global = output_switches3_y[f3, z1, z2, img]
+					# conv3 -> pool2
+					pool3_patches[f3, z1, z2, img] = max_output2[:, a3_x_global:a3_x_global+s3, a3_y_global:a3_y_global+s3, img]
+	
+	grad = np.zeros_like(F3)
+	for a3_x_ in range(s3):
+		for a3_y_ in range(s3):
+			for f2_ in range(n2):
+				pool3_deriv = pool3_patches[:,:,:,:,f2_,a3_x_,a3_y_]
+				for f3_ in range(n3):
+					pred_deriv = np.dot(FL[:,f3_].reshape((N_C, max_output_sz3**2)), pool3_deriv[f3_].reshape((max_output_sz3**2, N_IMGS))).T
+					
+					grad[f3_, f2_, a3_x_, a3_y_] = np.dot(pred_deriv, pred)
+					
+	F3 -= eps_F3 * grad
+	
+	########## FL deriv wrt cat_, f3_, z1_, z2_
+	grad = np.tile((pred[:,np.newaxis,np.newaxis,np.newaxis]*max_output3[np.newaxis]).sum(0).sum(-1)[np.newaxis], (999,1,1,1))
+	FL -= eps_FL * grad
 	
 	'''grad = L1_grad(F1, F2, F3, FL, output_switches3_x, output_switches3_y, output_switches2_x, output_switches2_y, output_switches1_x, output_switches1_y, s1, s2, s3, pred, Y, imgs_pad, sigma31, img_cats)
 	#print np.max(np.abs(grad))
