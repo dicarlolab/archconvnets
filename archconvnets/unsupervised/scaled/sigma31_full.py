ccfca60dc5c86ba4e5dc9aba6eb056640ada88d5
Darren Seibert <darren@munctional1.mit.edu>
2015-01-06 13:09:37 -0500

    full sigma

--- archconvnets/unsupervised/scaled/sigma31_full.py
+++ archconvnets/unsupervised/scaled/sigma31_full.py
@@ -2,8 +2,11 @@ import time
 import numpy as np
 from archconvnets.unsupervised.pool_inds import max_pool_locs
 from archconvnets.unsupervised.cudnn_module.cudnn_module import *
-from archconvnets.unsupervised.scaled.compute_sigma31 import s31
+from archconvnets.unsupervised.scaled.compute_sigma31_reduced import s31
+import archconvnets.unsupervised.sigma31_layers.sigma31_layers as sigma31_layers
 from scipy.io import savemat, loadmat
+from scipy.stats import zscore
+
 conv_block_cuda = conv
 F1_scale = 0.01 # std of init normal distribution
 F2_scale = 0.01
@@ -13,7 +16,7 @@ FL_scale = 0.3
 POOL_SZ = 3
 POOL_STRIDE = 2
 STRIDE1 = 1 # layer 1 stride
-N_IMGS = 32 # batch size
+N_IMGS = 1024 # batch size
 IMG_SZ_CROP = 28 # input image size (px)
 IMG_SZ = 32 # input image size (px)
 img_train_offset = 2
@@ -39,11 +42,16 @@ max_output_sz2  = len(range(0, output_sz2-POOL_SZ, POOL_STRIDE)) + 2*PAD
 output_sz3 = max_output_sz2 - s3 + 1
 max_output_sz3  = len(range(0, output_sz3-POOL_SZ, POOL_STRIDE))
 
-np.random.seed(666)
+np.random.seed(6666)
 F1 = np.single(np.random.normal(scale=F1_scale, size=(n1, 3, s1, s1)))
 F2 = np.single(np.random.normal(scale=F2_scale, size=(n2, n1, s2, s2)))
 F3 = np.single(np.random.normal(scale=F3_scale, size=(n3, n2, s3, s3)))
 FL = np.single(np.random.normal(scale=FL_scale, size=(N_C, n3, max_output_sz3, max_output_sz3)))
+
+F1 = zscore(F1,axis=None)/500
+F2 = zscore(F2,axis=None)/500
+F3 = zscore(F3,axis=None)/500
+FL = zscore(FL,axis=None)/500
 
 imgs_mean = np.load('/home/darren/cifar-10-py-colmajor/batches.meta')['data_mean']
 z = np.load('/home/darren/cifar-10-py-colmajor/data_batch_1')
@@ -55,9 +63,7 @@ x = z['data'] - imgs_mean
 x = x.reshape((3, 32, 32, 10000))
 x = x[:,:,:,:N_IMGS]
 
-l = np.zeros((N_IMGS, N_C),dtype='int')
-l[np.arange(N_IMGS),np.asarray(z['labels'])[:N_IMGS].astype(int)] = 1
-Y = np.single(l.T)
+labels = np.asarray(z['labels'])[:N_IMGS].astype(int)
 
 imgs_pad = np.zeros((3, IMG_SZ, IMG_SZ, N_IMGS),dtype='single')
 imgs_pad[:,PAD:PAD+IMG_SZ_CROP,PAD:PAD+IMG_SZ_CROP] = x[:,img_train_offset:img_train_offset+IMG_SZ_CROP,img_train_offset:img_train_offset+IMG_SZ_CROP]
@@ -86,8 +92,20 @@ output_switches3_y -= PAD
 output_switches2_x -= PAD
 output_switches2_y -= PAD
 
-##################3
+output_switches3_x[output_switches3_x < 0] = 0
+output_switches3_y[output_switches3_y < 0] = 0
+
+output_switches2_x[output_switches2_x < 0] = 0
+output_switches2_y[output_switches2_y < 0] = 0
+
+output_switches3_x[output_switches3_x >= max_output_sz3] = max_output_sz3-1
+output_switches3_y[output_switches3_y >= max_output_sz3] = max_output_sz3-1
+
+output_switches2_x[output_switches2_x >= max_output_sz2] = max_output_sz2-1
+output_switches2_y[output_switches2_y >= max_output_sz2] = max_output_sz2-1
+
+##################
 t_start = time.time()
-print N_IMGS, imgs_pad.shape
-sigma31 = s31(output_switches3_x, output_switches3_y, output_switches2_x, output_switches2_y, output_switches1_x, output_switches1_y, s1, s2, s3, Y, imgs_pad, N_C)
+sigma31 = sigma31_layers.s31_full(output_switches3_x, output_switches3_y, output_switches2_x, output_switches2_y, output_switches1_x, output_switches1_y, s1, s2, s3, labels, imgs_pad, N_C)
 print time.time() - t_start
+savemat('/home/darren/sigma31full_8N_1024imgs.mat',{'sigma31':sigma31})
