cdb1739084b6decbbd72a7ab01cb2204f8c6dfba
Darren Seibert <darren@munctional0.(none)>
2015-01-29 17:12:27 -0500

    m

--- archconvnets/unsupervised/sigma31_layers/sigma31_layers.py
+++ archconvnets/unsupervised/sigma31_layers/sigma31_layers.py
@@ -10,7 +10,7 @@ import numpy as np
 # labels [n_imgs]
 # imgs: [n_imgs, 3, img_sz, img_sz] (float32)
 # int: N_C
-def s31_full_gpu(output_switches3_x, output_switches3_y, output_switches2_x, output_switches2_y, output_switches1_x, output_switches1_y, s1, s2, s3, labels, imgs, N_C):
+def s31_full_gpu(output_switches3_x, output_switches3_y, output_switches2_x, output_switches2_y, output_switches1_x, output_switches1_y, s1, s2, s3, labels, imgs, N_C, warn=True):
 	assert isinstance(s1,int)
 	assert isinstance(s2,int)
 	assert isinstance(s3,int)
@@ -28,28 +28,35 @@ def s31_full_gpu(output_switches3_x, output_switches3_y, output_switches2_x, out
 	max_output_sz3 = output_switches3_x.shape[2]
 	
 	if not imgs.flags.contiguous:
-		print 'warning: input not C-contiguous (imgs)'
+		if warn:
+			print 'warning: input not C-contiguous (imgs)'
 		imgs = np.ascontiguousarray(imgs)
 	
 	if not output_switches3_x.flags.contiguous:
-		print 'warning: input not C-contiguous (output_switches3_x)'
+		if warn:
+			print 'warning: input not C-contiguous (output_switches3_x)'
 		output_switches3_x = np.ascontiguousarray(output_switches3_x)
 	if not output_switches3_y.flags.contiguous:
-		print 'warning: input not C-contiguous (output_switches3_y)'
+		if warn:
+			print 'warning: input not C-contiguous (output_switches3_y)'
 		output_switches3_y = np.ascontiguousarray(output_switches3_y)
 		
 	if not output_switches2_x.flags.contiguous:
-		print 'warning: input not C-contiguous (output_switches2_x)'
+		if warn:
+			print 'warning: input not C-contiguous (output_switches2_x)'
 		output_switches2_x = np.ascontiguousarray(output_switches2_x)
 	if not output_switches2_y.flags.contiguous:
-		print 'warning: input not C-contiguous (output_switches2_y)'
+		if warn:
+			print 'warning: input not C-contiguous (output_switches2_y)'
 		output_switches2_y = np.ascontiguousarray(output_switches2_y)
 		
 	if not output_switches1_x.flags.contiguous:
-		print 'warning: input not C-contiguous (output_switches1_x)'
+		if warn:
+			print 'warning: input not C-contiguous (output_switches1_x)'
 		output_switches1_x = np.ascontiguousarray(output_switches1_x)
 	if not output_switches1_y.flags.contiguous:
-		print 'warning: input not C-contiguous (output_switches1_y)'
+		if warn:
+			print 'warning: input not C-contiguous (output_switches1_y)'
 		output_switches1_y = np.ascontiguousarray(output_switches1_y)
 	
 	return _sigma31_layers.compute_sigma31_full_gpu(output_switches3_x, output_switches3_y, output_switches2_x, output_switches2_y, output_switches1_x, output_switches1_y, s1, s2, s3, labels, imgs, N_C)
