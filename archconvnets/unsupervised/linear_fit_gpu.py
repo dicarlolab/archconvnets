imgnet; fix additional ind signed overflow

archconvnets/unsupervised/linear_fit_gpu.py
@@ -60,7 +60,7 @@ FL = zscore(FL,axis=None)/500
 
 
 np.random.seed(6666)
-inds_keep = np.random.randint(n1*3*s1*s1*n2*s2*s2*n3*s3*s3*2*2, size=N_INDS_KEEP)
+inds_keep = np.random.randint(n1*3*s1*s1*n2*s2*s2*n3*s3*s3*max_output_sz3*max_output_sz3, size=N_INDS_KEEP)
 
 FL321 = F_prod_inds(F1, F2, F3, FL, inds_keep)
 

archconvnets/unsupervised/linear_fit_imgnet.py
@@ -1,15 +1,23 @@
 import numpy as np
 from scipy.stats import zscore
-from scipy.io import savemat
+from scipy.io import savemat, loadmat
 import time
 import random
+from archconvnets.unsupervised.sigma31_layers.sigma31_layers import F_prod_inds
+import gnumpy as gpu
 
+N = 48
 N_INDS_KEEP = 10000
+N_TEST_IMGS = 100
 
-sigma31 = np.load('/home/darren/s31_8.npy')
-sigma31_test_imgs = np.load('/home/darren/patches_8.npy')
+z = loadmat('/home/darren/sigmas_imgnet_' + str(N) + '_' + str(N_INDS_KEEP) + '.mat')
+y = loadmat('/home/darren/patches_imgnet_' + str(N) + '_' + str(N_INDS_KEEP) + '.mat')
 
-sigma11 = np.load('/home/darren/s11_8.npy')
+sigma31 = z['sigma31']
+sigma11 = z['sigma11']
+
+sigma31_test_imgs = y['patches'][:N_TEST_IMGS]
+labels = np.squeeze(y['labels'])[:N_TEST_IMGS]
 
 F1_scale = 0.01 # std of init normal distribution
 F2_scale = 0.01
@@ -19,10 +27,9 @@ FL_scale = 0.3
 POOL_SZ = 3
 POOL_STRIDE = 2
 STRIDE1 = 1 # layer 1 stride
-IMG_SZ = 32 # input image size (px)
+IMG_SZ = 138 # input image size (px)
 PAD = 2
 
-N = 16
 n1 = N # L1 filters
 n2 = N # ...
 n3 = N
@@ -31,11 +38,11 @@ s3 = 3 # L1 filter size (px)
 s2 = 5 # ...
 s1 = 5
 
-N_C = 10 # number of categories
+N_C = 999 # number of categories
 
 Y = np.eye(N_C)
 
-output_sz1 = len(range(0, IMG_SZ - s1 + 1, STRIDE1))
+output_sz1 = len(range(0, (IMG_SZ + 2*PAD) - s1 + 1, STRIDE1))
 max_output_sz1  = len(range(0, output_sz1-POOL_SZ, POOL_STRIDE)) + 2*PAD
 
 output_sz2 = max_output_sz1 - s2 + 1
@@ -46,8 +53,8 @@ max_output_sz3  = len(range(0, output_sz3-POOL_SZ, POOL_STRIDE))
 
 np.random.seed(6666)
 F1 = np.single(np.random.normal(scale=F1_scale, size=(n1, 3, s1, s1)))
-F2 = np.single(np.random.normal(scale=F2_scale, size=(n2, n1, s2, s2))).transpose((1,0,2,3))
-F3 = np.single(np.random.normal(scale=F3_scale, size=(n3, n2, s3, s3))).transpose((1,0,2,3))
+F2 = np.single(np.random.normal(scale=F2_scale, size=(n2, n1, s2, s2)))
+F3 = np.single(np.random.normal(scale=F3_scale, size=(n3, n2, s3, s3)))
 FL = np.single(np.random.normal(scale=FL_scale, size=(N_C, n3, max_output_sz3, max_output_sz3)))
 
 F1 = zscore(F1,axis=None)/500
@@ -55,23 +62,16 @@ F2 = zscore(F2,axis=None)/500
 F3 = zscore(F3,axis=None)/500
 FL = zscore(FL,axis=None)/500
 
-F1 = F1.reshape((  1, n1, 3, s1, s1,  1,  1,  1,  1,  1,  1,  1,  1))
-F2 = F2.reshape((  1, n1, 1,  1,  1, n2, s2, s2,  1,  1,  1,  1,  1))
-F3 = F3.reshape((  1,  1, 1,  1,  1, n2,  1,  1, n3, s3, s3,  1,  1))
-FL = FL.reshape((N_C,  1, 1,  1,  1,  1,  1,  1, n3,  1,  1,  max_output_sz3, max_output_sz3))
-
 np.random.seed(6666)
-inds_keep = np.random.randint(n1*3*s1*s1*n2*s2*s2*n3*s3*s3*2*2, size=N_INDS_KEEP)
+inds_keep = np.random.randint(n1*3*s1*s1*n2*s2*s2*n3*s3*s3*max_output_sz3*max_output_sz3, size=N_INDS_KEEP)
 
-FL321 = F1 * F2 * F3 * FL
-FL321 = FL321.reshape((N_C, np.prod(FL321.shape[1:])))[:,inds_keep]
+FL321 = F_prod_inds(F1, F2, F3, FL, inds_keep)
 
 sigma_inds = [0,2]
 F_inds = [1,2]
 
-EPS = 2.5e-13#2.5e-14
+EPS = 2.5e-18#2.5e-14
 
-labels = np.load('/home/darren/patches_8_labels.npy')
 Y_test = np.zeros((N_C, sigma31_test_imgs.shape[0]))
 Y_test[labels, range(sigma31_test_imgs.shape[0])] = 1
 
@@ -81,21 +81,26 @@ class_test = []
 err_train = []
 err_test = []
 
+sigma31_g = gpu.garray(sigma31)
+sigma11_g = gpu.garray(sigma11)
+
 print 'starting'
-#########
+########
 t_start = time.time()
 for step in range(100000):
-	grad = 2*(np.dot(FL321, sigma11) - sigma31)
+	FL321_g = gpu.garray(FL321)
 	
-	FL321 -= EPS * grad
+	grad = 2*(FL321_g.dot(sigma11_g) - sigma31_g)
+	
+	FL321 -= EPS * grad.as_numpy_array()
 	
 	if (step % 20) == 0:
 		pred = np.einsum(sigma31_test_imgs, sigma_inds, FL321, F_inds, [1,0])
 		err_test.append(np.mean((pred - Y_test)**2))
 		class_test.append((np.argmax(pred,axis=0) == labels).sum())
 		
-		print err_test[-1], 1 - class_test[-1]/10000.0, time.time() - t_start
-		savemat('/home/darren/linear_fit.mat', {'err_test': err_test, 'err_train': err_train, 
+		print err_test[-1], 1 - class_test[-1]/N_TEST_IMGS, time.time() - t_start
+		savemat('/home/darren/linear_fit_imgnet_' + str(N) + '_' + str(N_INDS_KEEP) +'.mat', {'err_test': err_test, 'err_train': err_train, 
 			'class_test': class_test, 'class_train': class_train})
 		t_start = time.time()
 	

archconvnets/unsupervised/scaled/sigma_rand_inds_imgnet.py
@@ -8,7 +8,7 @@ from scipy.stats import zscore
 import random
 import copy
 
-N_INDS_KEEP = 1000
+N_INDS_KEEP = 10000
 
 conv_block_cuda = cm.conv
 F1_scale = 0.01 # std of init normal distribution
@@ -19,9 +19,9 @@ FL_scale = 0.3
 POOL_SZ = 3
 POOL_STRIDE = 2
 STRIDE1 = 1 # layer 1 stride
-N_IMGS = 10000 # batch size
-IMG_SZ_CROP = 32 # input image size (px)
-IMG_SZ = 34#70#75# # input image size (px)
+N_IMGS = 128 # batch size
+IMG_SZ_CROP = 138 # input image size (px)
+IMG_SZ = 138#70#75# # input image size (px)
 img_train_offset = 0
 PAD = 2
 
@@ -34,9 +34,21 @@ s1 = 5
 s2 = 5
 s3 = 3
 
-N_C = 10 # number of categories
+N_C = 999 # number of categories
 
-output_sz1 = len(range(0, IMG_SZ - s1 + 1, STRIDE1))
+compute_training_sigmas = False
+if compute_training_sigmas:
+	file_name = '/home/darren/sigmas_imgnet_' + str(N) + '_' + str(N_INDS_KEEP) + '.mat'
+	batches = range(1,9100)
+else: # compute testing data (patches)
+	n_test_batches = 273
+	file_name = '/home/darren/patches_imgnet_' + str(N) + '_' + str(N_INDS_KEEP) + '.mat'
+	batches = range(9101,9101+n_test_batches)
+	
+	patches_keep = np.zeros((N_IMGS*n_test_batches, N_INDS_KEEP),dtype='single')
+	labels_keep = np.zeros((N_IMGS*n_test_batches),dtype='int')
+	
+output_sz1 = len(range(0, (IMG_SZ + 2*PAD) - s1 + 1, STRIDE1))
 max_output_sz1  = len(range(0, output_sz1-POOL_SZ, POOL_STRIDE)) + 2*PAD
 
 output_sz2 = max_output_sz1 - s2 + 1
@@ -60,20 +72,21 @@ F3 = zscore(F3,axis=None)/500
 sigma31 = np.zeros((N_C, N_INDS_KEEP), dtype='single')
 sigma11 = np.zeros((N_INDS_KEEP, N_INDS_KEEP), dtype='single')
 
-imgs_mean = np.load('/home/darren/cifar-10-py-colmajor/batches.meta')['data_mean']
-for batch in range(1,7):
+imgs_mean = np.load('/export/batch_storage2/batch128_img138_full/batches.meta')['data_mean'][:,np.newaxis]
+batch_ind = 0
+for batch in batches:
 	t_start = time.time()
-	z = np.load('/home/darren/cifar-10-py-colmajor/data_batch_' + str(batch))
+	z = np.load('/export/batch_storage2/batch128_img138_full/data_batch_' + str(batch))
 
 	################### compute train err
 	# load imgs
 	x = z['data'] - imgs_mean
-	x = x.reshape((3, 32, 32, 10000))
+	x = x.reshape((3, 138, 138, 128))
 	x = x[:,:,:,:N_IMGS]
 
 	labels = np.asarray(z['labels'])[:N_IMGS].astype(int)
 
-	imgs_pad = np.zeros((3, IMG_SZ, IMG_SZ, N_IMGS),dtype='single')
+	imgs_pad = np.zeros((3, IMG_SZ+2*PAD, IMG_SZ+2*PAD, N_IMGS),dtype='single')
 	imgs_pad[:,PAD:PAD+IMG_SZ_CROP,PAD:PAD+IMG_SZ_CROP] = x
 	imgs_pad = np.ascontiguousarray(imgs_pad.transpose((3,0,1,2)))
 
@@ -100,22 +113,38 @@ for batch in range(1,7):
 	output_switches3_x -= PAD
 	output_switches3_y -= PAD
 
-	print time.time() - t_forward_start
+	t_forward_start = time.time() - t_forward_start
 
 	t_patch = time.time()
 	patches  = patch_inds(output_switches3_x, output_switches3_y, output_switches2_x, output_switches2_y, output_switches1_x, output_switches1_y, s1, s2, s3, labels, imgs_pad, N_C, inds_keep, warn=False)
-	print time.time() - t_patch
+	t_patch = time.time() - t_patch
 	
-	if batch == 6:
-		break
+	# compute sigma11 & sigma31 or not...
+	if compute_training_sigmas:
+		t_sigma11 = time.time()
+		sigma11 += compute_sigma11_gpu(patches)
+		t_sigma11 = time.time() - t_sigma11
+
+		for cat in range(N_C):
+			sigma31[cat] += patches[labels == cat].sum(0)
 		
-	t_start = time.time()
-	sigma11 += compute_sigma11_gpu(patches)
-	print time.time() - t_start
+		if (batch % 20) == 0:
+			print 'saving...'
+			savemat(file_name,{'batch': batch,'sigma11':sigma11, 'sigma31':sigma31})
+		
+		print batch, time.time() - t_start, t_forward_start, t_patch, t_sigma11, file_name
+	else:
+		patches_keep[batch_ind*N_IMGS:(batch_ind+1)*N_IMGS] = copy.deepcopy(patches)
+		labels_keep[batch_ind*N_IMGS:(batch_ind+1)*N_IMGS] = copy.deepcopy(labels)
+		
+		if (batch % 20) == 0:
+			print 'saving...'
+			savemat(file_name,{'batch':batch, 'patches':patches_keep,'labels':labels_keep})
+			
+		print batch, time.time() - t_start, t_forward_start, file_name
+	batch_ind += 1
 
-	for cat in range(N_C):
-		sigma31[cat] += patches[labels == cat].sum(0)
-	print batch, time.time() - t_start
-
-
-savemat('/home/darren/sigmas_' + str(N) + '_' + str(N_INDS_KEEP) + '.mat',{'sigma11':sigma11, 'sigma31':sigma31, 'patches':patches,'labels':labels})
+if compute_training_sigmas:
+	savemat(file_name,{'batch': batch,'sigma11':sigma11, 'sigma31':sigma31})
+else:
+	savemat(file_name,{'batch':batch, 'patches':patches_keep,'labels':labels_keep})