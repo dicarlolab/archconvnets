98b9ebbc26d6578e43fdb3a409444ddde3e759cf
Darren Seibert <darren@munctional1.mit.edu>
2015-03-02 18:56:46 -0500

    conv constraint for all layers, separately

---
+++ archconvnets/unsupervised/linear_fit_cifar_conv_L1L2L3.py
@@ -0,0 +1,214 @@
+import numpy as np
+from scipy.stats import zscore
+from scipy.io import savemat, loadmat
+import time
+import random
+from archconvnets.unsupervised.sigma31_layers.sigma31_layers import F_prod_inds
+from scipy.stats import pearsonr
+
+N = 4
+N_INDS_KEEP = 10
+
+filename = '/home/darren/linear_fit_' + str(N) + '_' + str(N_INDS_KEEP) + '_conv.mat'
+
+z = loadmat('/home/darren/sigmas_train_test_' + str(N) + '_' + str(N_INDS_KEEP) + '_rand.mat')
+
+sigma31 = z['sigma31']
+sigma31_test_imgs = z['patches']
+labels = z['labels']
+sigma11 = z['sigma11']
+inds_keep = np.squeeze(z['inds_keep'])
+
+F1_scale = 0.01 # std of init normal distribution
+F2_scale = 0.01
+F3_scale = 0.01
+FL_scale = 0.3
+
+POOL_SZ = 3
+POOL_STRIDE = 2
+STRIDE1 = 1 # layer 1 stride
+IMG_SZ = 34 # input image size (px)
+PAD = 2
+
+n1 = N # L1 filters
+n2 = N # ...
+n3 = N
+
+s3 = 3 # L1 filter size (px)
+s2 = 5 # ...
+s1 = 5
+
+N_C = 10 # number of categories
+
+Y = np.eye(N_C)
+
+output_sz1 = len(range(0, IMG_SZ - s1 + 1, STRIDE1))
+max_output_sz1  = len(range(0, output_sz1-POOL_SZ, POOL_STRIDE)) + 2*PAD
+
+output_sz2 = max_output_sz1 - s2 + 1
+max_output_sz2  = len(range(0, output_sz2-POOL_SZ, POOL_STRIDE)) + 2*PAD
+
+output_sz3 = max_output_sz2 - s3 + 1
+max_output_sz3  = len(range(0, output_sz3-POOL_SZ, POOL_STRIDE))
+
+np.random.seed(660662)
+F1 = np.single(np.random.normal(scale=F1_scale, size=(n1, 3, s1, s1)))
+F2 = np.single(np.random.normal(scale=F2_scale, size=(n2, n1, s2, s2)))
+F3 = np.single(np.random.normal(scale=F3_scale, size=(n3, n2, s3, s3)))
+FL = np.single(np.random.normal(scale=FL_scale, size=(N_C, n3, max_output_sz3, max_output_sz3)))
+
+F1 = zscore(F1,axis=None)/500
+F2 = zscore(F2,axis=None)/500
+F3 = zscore(F3,axis=None)/500
+FL = zscore(FL,axis=None)/500
+
+FL321 = F_prod_inds(F1, F2, F3, FL, inds_keep)
+fl321s = FL321.shape
+FL321 = FL321.ravel()
+#FL321 = np.ones_like(FL321)
+#random.shuffle(FL321)
+FL321 = FL321.reshape(fl321s)
+
+sigma_inds = [0,2]
+F_inds = [1,2]
+
+EPS = 1e-12
+EPS_CORR = 1e-8
+
+Y_test = np.zeros((N_C, sigma31_test_imgs.shape[0]))
+Y_test[labels, range(sigma31_test_imgs.shape[0])] = 1
+
+class_train = []
+class_test = []
+convolutionarity_F1 = []
+convolutionarity_F2 = []
+convolutionarity_F3 = []
+err_train = []
+err_test = []
+
+import gnumpy as gpu
+sigma31_g = gpu.garray(sigma31)
+sigma11_g = gpu.garray(sigma11)
+
+F1_s = np.prod(F1.shape)
+F2_s = np.prod(F2.shape)
+F3_s = np.prod(F3.shape)
+
+print 'starting'
+########
+step = 0
+t_start = time.time()
+
+while True:
+	FL321_g = gpu.garray(FL321)
+	
+	grad = 2*(FL321_g.dot(sigma11_g) - sigma31_g)
+	grad_corrs = np.zeros_like(FL321)
+	
+	################### F1
+	FL321t = FL321[0,:N_INDS_KEEP*F1_s].reshape((4*3*5*5, N_INDS_KEEP))
+	
+	FL321t_no_mean = FL321t - FL321t.mean(0)[np.newaxis]
+	FL321t_sigma = np.std(FL321t,0) * np.sqrt(FL321t.shape[0])
+	FL321t_no_mean_no_sigma = FL321t_no_mean / FL321t_sigma[np.newaxis]
+	FL321t_sigma2 = FL321t_sigma**2
+	
+	grad_corr = np.zeros((4*3*5*5, N_INDS_KEEP))
+	total = 0
+	for i in range(N_INDS_KEEP):
+		for j in range(i+1,N_INDS_KEEP):
+			dp = np.dot(FL321t_no_mean[:,i], FL321t_no_mean[:,j])
+			
+			grad_corr[:,i] += ((FL321t_no_mean[:,j] * FL321t_sigma[i]) - (dp * FL321t_no_mean_no_sigma[:,i])) / (FL321t_sigma2[i] * FL321t_sigma[j])
+			grad_corr[:,j] += ((FL321t_no_mean[:,i] * FL321t_sigma[j]) - (dp * FL321t_no_mean_no_sigma[:,j])) / (FL321t_sigma2[j] * FL321t_sigma[i])
+			
+			total += 1
+	
+	grad_corrs[:,:N_INDS_KEEP*F1_s] += (grad_corr.ravel())[np.newaxis] / total
+	
+	################## F2
+	FL321t = FL321[0, N_INDS_KEEP*F1_s:N_INDS_KEEP*(F1_s + F2_s)].reshape((4*4*5*5, N_INDS_KEEP))
+	
+	FL321t_no_mean = FL321t - FL321t.mean(0)[np.newaxis]
+	FL321t_sigma = np.std(FL321t,0) * np.sqrt(FL321t.shape[0])
+	FL321t_no_mean_no_sigma = FL321t_no_mean / FL321t_sigma[np.newaxis]
+	FL321t_sigma2 = FL321t_sigma**2
+	
+	grad_corr = np.zeros((4*4*5*5, N_INDS_KEEP))
+	total = 0
+	for i in range(N_INDS_KEEP):
+		for j in range(i+1,N_INDS_KEEP):
+			dp = np.dot(FL321t_no_mean[:,i], FL321t_no_mean[:,j])
+			
+			grad_corr[:,i] += ((FL321t_no_mean[:,j] * FL321t_sigma[i]) - (dp * FL321t_no_mean_no_sigma[:,i])) / (FL321t_sigma2[i] * FL321t_sigma[j])
+			grad_corr[:,j] += ((FL321t_no_mean[:,i] * FL321t_sigma[j]) - (dp * FL321t_no_mean_no_sigma[:,j])) / (FL321t_sigma2[j] * FL321t_sigma[i])
+			
+			total += 1
+	grad_corrs[:, N_INDS_KEEP*F1_s:N_INDS_KEEP*(F1_s + F2_s)] += (grad_corr.ravel())[np.newaxis] / total
+	
+	############## F3
+	FL321t = FL321[0, N_INDS_KEEP*(F1_s + F2_s):].reshape((4*4*3*3, N_INDS_KEEP))
+	
+	FL321t_no_mean = FL321t - FL321t.mean(0)[np.newaxis]
+	FL321t_sigma = np.std(FL321t,0) * np.sqrt(FL321t.shape[0])
+	FL321t_no_mean_no_sigma = FL321t_no_mean / FL321t_sigma[np.newaxis]
+	FL321t_sigma2 = FL321t_sigma**2
+	
+	grad_corr = np.zeros((4*4*3*3, N_INDS_KEEP))
+	total = 0
+	for i in range(N_INDS_KEEP):
+		for j in range(i+1,N_INDS_KEEP):
+			dp = np.dot(FL321t_no_mean[:,i], FL321t_no_mean[:,j])
+			
+			grad_corr[:,i] += ((FL321t_no_mean[:,j] * FL321t_sigma[i]) - (dp * FL321t_no_mean_no_sigma[:,i])) / (FL321t_sigma2[i] * FL321t_sigma[j])
+			grad_corr[:,j] += ((FL321t_no_mean[:,i] * FL321t_sigma[j]) - (dp * FL321t_no_mean_no_sigma[:,j])) / (FL321t_sigma2[j] * FL321t_sigma[i])
+			
+			total += 1
+	grad_corrs[:, N_INDS_KEEP*(F1_s + F2_s):] += grad_corr.ravel()[np.newaxis] / total
+	
+	FL321 -= EPS * grad.as_numpy_array()
+	FL321 += EPS_CORR * grad_corrs
+	
+	if (step % 20) == 0:
+		pred = np.einsum(sigma31_test_imgs, sigma_inds, FL321, F_inds, [1,0])
+		err_test.append(np.mean((pred - Y_test)**2))
+		class_test.append((np.argmax(pred,axis=0) == labels).sum())
+		
+		### convolutionarity for layer 1
+		FL321t = FL321[0,:N_INDS_KEEP*F1_s].reshape((3*4*5*5, N_INDS_KEEP))
+
+		intact_sum = 0
+		total = 0
+		for i in range(N_INDS_KEEP):
+			for j in range(i+1,N_INDS_KEEP):
+				intact_sum += np.abs(pearsonr(FL321t[:,i], FL321t[:,j])[0])
+				total += 1
+		convolutionarity_F1.append(intact_sum/total)
+		
+		### convolutionarity for layer 2
+		FL321t = FL321[0,N_INDS_KEEP*F1_s:N_INDS_KEEP*(F1_s + F2_s)].reshape((4*4*5*5, N_INDS_KEEP))
+
+		intact_sum = 0
+		total = 0
+		for i in range(N_INDS_KEEP):
+			for j in range(i+1,N_INDS_KEEP):
+				intact_sum += np.abs(pearsonr(FL321t[:,i], FL321t[:,j])[0])
+				total += 1
+		convolutionarity_F2.append(intact_sum/total)
+		
+		### convolutionarity for layer 3
+		FL321t = FL321[0,N_INDS_KEEP*(F1_s + F2_s):].reshape((4*4*3*3, N_INDS_KEEP))
+
+		intact_sum = 0
+		total = 0
+		for i in range(N_INDS_KEEP):
+			for j in range(i+1,N_INDS_KEEP):
+				intact_sum += np.abs(pearsonr(FL321t[:,i], FL321t[:,j])[0])
+				total += 1
+		convolutionarity_F3.append(intact_sum/total)
+		
+		print err_test[-1], 1 - class_test[-1]/10000.0, time.time() - t_start, convolutionarity_F1[-1], convolutionarity_F2[-1], convolutionarity_F3[-1], filename
+		savemat(filename, {'err_test': err_test, 'err_train': err_train, 
+			'class_test': class_test, 'class_train': class_train, 'convolutionarity_F1':convolutionarity_F1, 'convolutionarity_F2':convolutionarity_F2, 'convolutionarity_F3':convolutionarity_F3})
+		t_start = time.time()
+	
---
+++ archconvnets/unsupervised/sigma_rand_inds_cifar_even_sampleL1L2L3.py
@@ -0,0 +1,162 @@
+import time
+import numpy as np
+from archconvnets.unsupervised.cudnn_module import cudnn_module as cm
+import archconvnets.unsupervised.sigma31_layers.sigma31_layers as sigma31_layers
+from archconvnets.unsupervised.sigma31_layers.sigma31_layers import max_pool_locs, patch_inds, compute_sigma11_gpu
+from scipy.io import savemat, loadmat
+from scipy.stats import zscore
+import random
+import copy
+
+N_INDS_KEEP = 30
+
+conv_block_cuda = cm.conv
+F1_scale = 0.01 # std of init normal distribution
+F2_scale = 0.01
+F3_scale = 0.01
+FL_scale = 0.3
+
+POOL_SZ = 3
+POOL_STRIDE = 2
+STRIDE1 = 1 # layer 1 stride
+N_IMGS = 10000 # batch size
+IMG_SZ_CROP = 32 # input image size (px)
+IMG_SZ = 34#70#75# # input image size (px)
+img_train_offset = 0
+PAD = 2
+
+N = 4
+n1 = N # L1 filters
+n2 = N
+n3 = N
+
+s1 = 5
+s2 = 5
+s3 = 3
+
+N_C = 10 # number of categories
+
+output_sz1 = len(range(0, IMG_SZ - s1 + 1, STRIDE1))
+max_output_sz1  = len(range(0, output_sz1-POOL_SZ, POOL_STRIDE)) + 2*PAD
+
+output_sz2 = max_output_sz1 - s2 + 1
+max_output_sz2  = len(range(0, output_sz2-POOL_SZ, POOL_STRIDE)) + 2*PAD
+
+output_sz3 = max_output_sz2 - s3 + 1
+max_output_sz3  = len(range(0, output_sz3-POOL_SZ, POOL_STRIDE))
+
+np.random.seed(660662)
+#inds_keep = np.random.randint(n1*3*s1*s1*n2*s2*s2*n3*s3*s3*max_output_sz3*max_output_sz3, size=N_INDS_KEEP)
+
+###########
+# dense sample points with even spread over F1 values
+remainder_inds = np.random.randint(4*5*5*4*3*3*2*2, size=N_INDS_KEEP)
+remainder_inds_unraveled = np.asarray(np.unravel_index(remainder_inds, (4,5,5,4,3,3,2,2)))
+
+global_inds = np.zeros((12, n1*3*5*5*N_INDS_KEEP),dtype='int')
+for f_ind in range(n1*3*5*5):
+	f_inds_unraveled = np.asarray(np.unravel_index(f_ind, (n1, 3, 5, 5)))
+	global_inds[:, f_ind*N_INDS_KEEP:(f_ind+1)*N_INDS_KEEP] = np.concatenate((np.tile(f_inds_unraveled[:,np.newaxis], (1, N_INDS_KEEP)), remainder_inds_unraveled), axis=0)
+
+inds_keepL1 = np.ravel_multi_index(global_inds, (n1,3,5,5,4,5,5,4,3,3,2,2))
+
+###########
+# dense sample points with even spread over F2 values
+remainder_inds = np.random.randint(3*5*5*4*3*3*2*2, size=N_INDS_KEEP)
+remainder_inds_unraveled = np.asarray(np.unravel_index(remainder_inds, (3,5,5,4,3,3,2,2)))
+
+global_inds = np.zeros((12, n2*n1*5*5*N_INDS_KEEP),dtype='int')
+for f_ind in range(n2*n1*5*5):
+	f_inds_unraveled = np.asarray(np.unravel_index(f_ind, (n2, n1, 5, 5)))
+	global_inds[:, f_ind*N_INDS_KEEP:(f_ind+1)*N_INDS_KEEP] = np.concatenate((np.tile(f_inds_unraveled[:,np.newaxis], (1, N_INDS_KEEP)), remainder_inds_unraveled), axis=0)
+
+global_inds = global_inds[[1, 4, 5, 6, 0, 2, 3, 7, 8, 9, 10, 11]] # put F1 indices first
+inds_keepL2 = np.ravel_multi_index(global_inds, (n1,3,5,5,n2,5,5,4,3,3,2,2))
+
+###########
+# dense sample points with even spread over F3 values
+remainder_inds = np.random.randint(4*3*5*5*5*5*2*2, size=N_INDS_KEEP)
+remainder_inds_unraveled = np.asarray(np.unravel_index(remainder_inds, (4,3,5,5,5,5,2,2)))
+
+global_inds = np.zeros((12, n3*n2*3*3*N_INDS_KEEP),dtype='int')
+for f_ind in range(n3*n2*3*3):
+	f_inds_unraveled = np.asarray(np.unravel_index(f_ind, (n3, n2, 3, 3)))
+	global_inds[:, f_ind*N_INDS_KEEP:(f_ind+1)*N_INDS_KEEP] = np.concatenate((np.tile(f_inds_unraveled[:,np.newaxis], (1, N_INDS_KEEP)), remainder_inds_unraveled), axis=0)
+
+global_inds = global_inds[[4, 5, 6, 7, 1, 8, 9, 0, 2, 3, 10, 11]] # put F1 indices first
+inds_keepL3 = np.ravel_multi_index(global_inds, (n1,3,5,5,n2,5,5,4,3,3,2,2))
+
+inds_keep = np.concatenate((inds_keepL1, inds_keepL2, inds_keepL3))
+
+#####
+np.random.seed(66066)
+F1 = np.single(np.random.normal(scale=F1_scale, size=(n1, 3, s1, s1)))
+F2 = np.single(np.random.normal(scale=F2_scale, size=(n2, n1, s2, s2)))
+F3 = np.single(np.random.normal(scale=F3_scale, size=(n3, n2, s3, s3)))
+
+F1 = zscore(F1,axis=None)/500
+F2 = zscore(F2,axis=None)/500
+F3 = zscore(F3,axis=None)/500
+
+sigma31 = np.zeros((N_C, len(inds_keep)), dtype='single')
+sigma11 = np.zeros((len(inds_keep), len(inds_keep)), dtype='single')
+
+imgs_mean = np.load('/home/darren/cifar-10-py-colmajor/batches.meta')['data_mean']
+for batch in [1,6]:#range(1,7):
+	t_start = time.time()
+	z = np.load('/home/darren/cifar-10-py-colmajor/data_batch_' + str(batch))
+
+	################### compute train err
+	# load imgs
+	x = z['data'] - imgs_mean
+	x = x.reshape((3, 32, 32, 10000))
+	x = x[:,:,:,:N_IMGS]
+
+	labels = np.asarray(z['labels'])[:N_IMGS].astype(int)
+
+	imgs_pad = np.zeros((3, IMG_SZ, IMG_SZ, N_IMGS),dtype='single')
+	imgs_pad[:,PAD:PAD+IMG_SZ_CROP,PAD:PAD+IMG_SZ_CROP] = x
+	imgs_pad = np.ascontiguousarray(imgs_pad.transpose((3,0,1,2)))
+
+	# forward pass
+	t_forward_start = time.time()
+	conv_output1 = conv_block_cuda(F1, imgs_pad)
+	max_output1t, output_switches1_x, output_switches1_y = max_pool_locs(np.single(conv_output1),warn=False)
+
+	max_output1 = np.zeros((N_IMGS, n1, max_output_sz1, max_output_sz1),dtype='single')
+	max_output1[:,:,PAD:max_output_sz1-PAD,PAD:max_output_sz1-PAD] = max_output1t
+
+	conv_output2 = conv_block_cuda(F2, max_output1)
+	max_output2t, output_switches2_x, output_switches2_y = max_pool_locs(np.single(conv_output2), PAD=2,warn=False)
+
+	max_output2 = np.zeros((N_IMGS, n2, max_output_sz2, max_output_sz2),dtype='single')
+	max_output2[:,:,PAD:max_output_sz2-PAD,PAD:max_output_sz2-PAD] = max_output2t
+
+	conv_output3 = conv_block_cuda(F3, max_output2)
+	max_output3t, output_switches3_x, output_switches3_y = max_pool_locs(np.single(conv_output3), PAD=2,warn=False)
+
+	output_switches2_x -= PAD
+	output_switches2_y -= PAD
+
+	output_switches3_x -= PAD
+	output_switches3_y -= PAD
+
+	print time.time() - t_forward_start
+
+	t_patch = time.time()
+	patches  = patch_inds(output_switches3_x, output_switches3_y, output_switches2_x, output_switches2_y, output_switches1_x, output_switches1_y, s1, s2, s3, labels, imgs_pad, N_C, inds_keep, warn=False)
+	print time.time() - t_patch
+	
+	if batch == 6:
+		break
+		
+	t_start = time.time()
+	sigma11 += compute_sigma11_gpu(patches)
+	print time.time() - t_start
+
+	for cat in range(N_C):
+		sigma31[cat] += patches[labels == cat].sum(0)
+	print batch, time.time() - t_start
+
+
+savemat('/home/darren/sigmas_train_test_' + str(N) + '_' + str(N_INDS_KEEP) + '_rand.mat',{'sigma11':sigma11, 'sigma31':sigma31, 'patches':patches,'labels':labels, 'inds_keep': inds_keep,'N_INDS_KEEP':N_INDS_KEEP})
