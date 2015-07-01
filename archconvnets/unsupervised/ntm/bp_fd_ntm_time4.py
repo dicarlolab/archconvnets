cf4d577fe299a757d27444e5f3e360a828737a98
Darren Seibert <darren@munctional1.mit.edu>
2015-06-30 20:31:54 -0400

    rtrl multi-layer debugging

--- archconvnets/unsupervised/ntm/bp_fd_ntm_time4.py
+++ archconvnets/unsupervised/ntm/bp_fd_ntm_time4.py
@@ -10,17 +10,21 @@ from ntm_gradients import *
 
 C = 4
 M = 5
+n1 = 6
 n_in = 3
 
 t = np.random.normal(size=(C,M))
 o_previ = np.random.normal(size=(C,M))
 g = np.random.normal(size=(C,1))
-w = np.random.normal(size=(C,n_in))
+
+w2 = np.random.normal(size=(C,n1))
+w1 = np.random.normal(size=(n1,n_in))
+
 x = np.random.normal(size=(n_in,1))
 x2 = np.random.normal(size=(n_in,1))
 x3 = np.random.normal(size=(n_in,1))
 
-do_dgi = np.zeros((C,M,n_in))
+do_dw2i = np.zeros((C,M,n1))
 
 ################# interpolate simplified
 def interpolate_simp(w_prev, interp_gate_out):
@@ -31,56 +35,60 @@ def interpolate_simp_dinterp_gate_out_partial(interp_gate_out, dprev):
 	return dprev
 
 def f(y):
-	w[i_ind,j_ind] = y
+	w2[i_ind,j_ind] = y
 	o_prev = copy.deepcopy(o_previ)
 	
 	###
-	g = linear_F(w,x)
-	o = interpolate_simp(o_prev, g)
+	g1 = linear_F(w1,x)
+	g2 = linear_F(w2,g1)
+	o = interpolate_simp(o_prev, g2)
 	
-	o_prev = copy.deepcopy(o)
+	'''o_prev = copy.deepcopy(o)
 	
 	###
-	g = linear_F(w,x2)
-	o = interpolate_simp(o_prev, g)
+	g1 = linear_F(w1,x2)
+	g2 = linear_F(w2,g1)
+	o = interpolate_simp(o_prev, g2)
 	
-	o_prev = copy.deepcopy(o)
+	o_prev = copy.deepcopy(o)'''
 	
 	return ((o - t)**2).sum()
 
 
 def g(y):
-	w[i_ind,j_ind] = y
-	do_dg = copy.deepcopy(do_dgi)
+	w2[i_ind,j_ind] = y
+	do_dw2 = copy.deepcopy(do_dw2i)
 	o_prev = copy.deepcopy(o_previ)
 	
 	###
-	g = linear_F(w,x)
-	o = interpolate_simp(o_prev, g)
+	g1 = linear_F(w1,x)
+	g2 = linear_F(w2,g1)
+	o = interpolate_simp(o_prev, g2)
 	
-	dg_dw = linear_dF(x,1).T
+	dg_dw2 = linear_dF(g1,1).T
 	
-	do_dg = np.einsum(do_dg, [0,1,2], g, [0,3], [0,1,2]) # g * do_dw
-	do_dg += np.einsum(o_prev, [0,1], dg_dw, [2,3], [0,1,2]) # x * o^(t-1)
+	do_dw2 = np.einsum(do_dw2, [0,1,2], g2, [0,3], [0,1,2]) # g * do_dw
+	do_dw2 += np.einsum(o_prev, [0,1], dg_dw2, [2,3], [0,1,2]) # x * o^(t-1)
+	
+	'''o_prev = copy.deepcopy(o)
+	
+	###
+	g1 = linear_F(w1,x2)
+	g2 = linear_F(w2,g1)
+	o = interpolate_simp(o_prev, g2)
+	
+	dg_dw2 = linear_dF(g1,1).T
+	
+	do_dw2 = np.einsum(do_dw2, [0,1,2], g2, [0,3], [0,1,2]) # g * do_dw
+	do_dw2 += np.einsum(o_prev, [0,1], dg_dw2, [2,3], [0,1,2]) # x * o^(t-1)
 	
 	o_prev = copy.deepcopy(o)
 	
-	###
-	g = linear_F(w,x2)
-	o = interpolate_simp(o_prev, g)
+	###'''
 	
-	dg_dw = linear_dF(x2,1).T
+	dw2 = np.einsum(2*(o - t), [0,1], do_dw2, [0,1,2], [0,2])
 	
-	do_dg = np.einsum(do_dg, [0,1,2], g, [0,3], [0,1,2]) # g * do_dw
-	do_dg += np.einsum(o_prev, [0,1], dg_dw, [2,3], [0,1,2]) # x * o^(t-1)
-	
-	o_prev = copy.deepcopy(o)
-	
-	###
-	
-	dw = np.einsum(2*(o - t), [0,1], do_dg, [0,1,2], [0,2])
-	
-	return dw[i_ind,j_ind]
+	return dw2[i_ind,j_ind]
 	
 	
 np.random.seed(np.int64(time.time()))
@@ -91,7 +99,7 @@ N_SAMPLES = 25
 ratios = np.zeros(N_SAMPLES)
 for sample in range(N_SAMPLES):
 
-	ref = w
+	ref = w2
 	i_ind = np.random.randint(ref.shape[0])
 	j_ind = np.random.randint(ref.shape[1])
 	y = -1e0*ref[i_ind,j_ind]; gt = g(y); gtx = scipy.optimize.approx_fprime(np.ones(1)*y, f, eps)
