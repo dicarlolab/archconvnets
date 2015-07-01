e846d9925b9fbc3d878aa129567de8e540af70ac
Darren Seibert <darren@munctional1.mit.edu>
2015-07-01 00:59:45 -0400

    dimensionality reduction for partial storage

--- archconvnets/unsupervised/ntm/bp_fd_ntm_time2.py
+++ archconvnets/unsupervised/ntm/bp_fd_ntm_time2.py
@@ -27,7 +27,7 @@ x2 = np.random.normal(size=(n_in,1))
 
 do_dw3i = np.zeros((C,M,n2))
 do_dw2i = np.zeros((C,M,n2,n1))
-do_dw1i = np.zeros((C,M,n2,n1,n_in))
+do_dw1i = np.zeros((C,M,n1,n_in))
 
 ################# interpolate simplified
 def interpolate_simp(w_prev, interp_gate_out):
@@ -80,8 +80,8 @@ def g(y):
 	dg3_dg1 = np.einsum(dg3_dg2, [0,1], dg2_dg1, [1,2], [0,1,2])
 	dg3_dw1 = np.einsum(dg3_dg1, [0,1,2], dg1_dw1, [3,4], [0,1,2,3])
 	
-	do_dw1 = np.einsum(do_dw1, range(5), g3, [0,5], range(5))
-	do_dw1 += np.einsum(o_prev, [0,1], dg3_dw1, [0,2,3,4], range(5))
+	do_dw1 = np.einsum(do_dw1, range(4), g3, [0,4], range(4))
+	do_dw1 += np.einsum(o_prev, [0,1], dg3_dw1, [0,4,2,3], range(4))
 	
 	# w2:
 	dg2_dw2 = g1
@@ -113,8 +113,8 @@ def g(y):
 	dg3_dg1 = np.einsum(dg3_dg2, [0,1], dg2_dg1, [1,2], [0,1,2])
 	dg3_dw1 = np.einsum(dg3_dg1, [0,1,2], dg1_dw1, [3,4], [0,1,2,3])
 	
-	do_dw1 = np.einsum(do_dw1, range(5), g3, [0,5], range(5))
-	do_dw1 += np.einsum(o_prev, [0,1], dg3_dw1, [0,2,3,4], range(5))
+	do_dw1 = np.einsum(do_dw1, range(4), g3, [0,4], range(4))
+	do_dw1 += np.einsum(o_prev, [0,1], dg3_dw1, [0,4,2,3], range(4))
 	
 	# w2:
 	dg2_dw2 = g1
@@ -134,7 +134,7 @@ def g(y):
 	
 	###
 	
-	dw1 = np.einsum(2*(o - t), [0,1], do_dw1, range(5), [3,4])
+	dw1 = np.einsum(2*(o - t), [0,1], do_dw1, range(4), [2,3])
 	dw2 = np.einsum(2*(o - t), [0,1], do_dw2, range(4), [2,3])
 	dw3 = np.einsum(2*(o - t), [0,1], do_dw3, [0,1,2], [0,2])
 	
