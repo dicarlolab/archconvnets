aaba0f13acc2c924fba96fd7f859a96173f10846
Darren Seibert <darren@munctional1.mit.edu>
2015-07-01 17:46:18 -0400

    expanded interpolation test

--- archconvnets/unsupervised/ntm/bp_fd_ntm_time.py
+++ archconvnets/unsupervised/ntm/bp_fd_ntm_time.py
@@ -16,7 +16,7 @@ n_in = 3
 
 t = np.random.normal(size=(C,M))
 o_previ = np.random.normal(size=(C,M))
-g = np.random.normal(size=(C,1))
+o_content = np.random.normal(size=(C,M))
 
 w3 = np.random.normal(size=(C,n2))
 w2 = np.random.normal(size=(n2,n1))
@@ -28,6 +28,10 @@ x2 = np.random.normal(size=(n_in,1))
 do_dw3i = np.zeros((C,M,n2))
 do_dw2i = np.zeros((C,M,n2,n1))
 do_dw1i = np.zeros((C,M,n1,n_in))
+
+do_content_dw3 = np.zeros_like(do_dw3i)
+do_content_dw2 = np.zeros_like(do_dw2i)
+do_content_dw1 = np.zeros_like(do_dw1i)
 
 ################# interpolate simplified
 def interpolate_simp(w_prev, interp_gate_out):
@@ -44,6 +48,7 @@ def f(y):
 	g2 = sq_F(w2,g1)
 	g3 = sq_F(w3,g2)
 	o = interpolate_simp(o_prev, g3)
+	o += interpolate_simp(o_content, g3)
 	
 	o_prev = copy.deepcopy(o)
 	
@@ -52,6 +57,7 @@ def f(y):
 	g2 = sq_F(w2,g1)
 	g3 = sq_F(w3,g2)
 	o = interpolate_simp(o_prev, g3)
+	o += interpolate_simp(o_content, g3)
 	
 	o_prev = copy.deepcopy(o)
 	
@@ -71,6 +77,7 @@ def g(y):
 	g2 = sq_F(w2,g1)
 	g3 = sq_F(w3,g2)
 	o = interpolate_simp(o_prev, g3)
+	o += interpolate_simp(o_content, g3)
 	
 	# w1:
 	dg3_dg2 = sq_dlayer_in_nsum(w3, g2)
@@ -78,24 +85,24 @@ def g(y):
 	dg1_dw1 = sq_dF(w1, x, g1)
 	
 	dg3_dg1 = np.einsum(dg3_dg2, [0,1], dg2_dg1, [1,2], [0,1,2])
-	dg3_dw1 = np.einsum(dg3_dg1, [0,1,2], dg1_dw1, [2,3], [0,1,2,3])
+	dg3_dw1 = np.einsum(dg3_dg1, [0,1,2], dg1_dw1, [2,3], range(4))
 	
-	do_dw1 = np.einsum(do_dw1, range(4), g3, [0,4], range(4))
-	do_dw1 += np.einsum(o_prev, [0,1], dg3_dw1, [0,4,2,3], range(4))
+	do_dw1 = np.einsum(do_dw1 + do_content_dw1, range(4), g3, [0,4], range(4))
+	do_dw1 += np.einsum(o_prev + o_content, [0,1], dg3_dw1, [0,4,2,3], range(4))
 	
 	# w2:
 	dg2_dw2 = sq_dF(w2, g1, g2)
 	
 	dg3_dw2 = np.einsum(dg3_dg2,[0,1], dg2_dw2, [1,2], [0,1,2])
 	
-	do_dw2 = np.einsum(do_dw2, range(4), g3, [0,3], range(4))
-	do_dw2 += np.einsum(o_prev, [0,1], dg3_dw2, [0,2,3], range(4))
+	do_dw2 = np.einsum(do_dw2 + do_content_dw2, range(4), g3, [0,3], range(4))
+	do_dw2 += np.einsum(o_prev + o_content, [0,1], dg3_dw2, [0,2,3], range(4))
 	
 	# w3:
 	dg3_dw3 = sq_dF(w3, g2, g3)
 	
-	do_dw3 = np.einsum(do_dw3, [0,1,2], g3, [0,3], [0,1,2])
-	do_dw3 += np.einsum(o_prev, [0,1], dg3_dw3, [0,2], [0,1,2])
+	do_dw3 = np.einsum(do_dw3 + do_content_dw3, [0,1,2], g3, [0,3], [0,1,2])
+	do_dw3 += np.einsum(o_prev + o_content, [0,1], dg3_dw3, [0,2], [0,1,2])
 	
 	o_prev = copy.deepcopy(o)
 	
@@ -104,6 +111,7 @@ def g(y):
 	g2 = sq_F(w2,g1)
 	g3 = sq_F(w3,g2)
 	o = interpolate_simp(o_prev, g3)
+	o += interpolate_simp(o_content, g3)
 	
 	# w1:
 	dg3_dg2 = sq_dlayer_in_nsum(w3, g2)
@@ -111,24 +119,24 @@ def g(y):
 	dg1_dw1 = sq_dF(w1, x2, g1)
 	
 	dg3_dg1 = np.einsum(dg3_dg2, [0,1], dg2_dg1, [1,2], [0,1,2])
-	dg3_dw1 = np.einsum(dg3_dg1, [0,1,2], dg1_dw1, [2,3], [0,1,2,3])
+	dg3_dw1 = np.einsum(dg3_dg1, [0,1,2], dg1_dw1, [2,3], range(4))
 	
-	do_dw1 = np.einsum(do_dw1, range(4), g3, [0,4], range(4))
-	do_dw1 += np.einsum(o_prev, [0,1], dg3_dw1, [0,4,2,3], range(4))
+	do_dw1 = np.einsum(do_dw1 + do_content_dw1, range(4), g3, [0,4], range(4))
+	do_dw1 += np.einsum(o_prev + o_content, [0,1], dg3_dw1, [0,4,2,3], range(4))
 	
 	# w2:
 	dg2_dw2 = sq_dF(w2, g1, g2)
 	
 	dg3_dw2 = np.einsum(dg3_dg2,[0,1], dg2_dw2, [1,2], [0,1,2])
 	
-	do_dw2 = np.einsum(do_dw2, range(4), g3, [0,3], range(4))
-	do_dw2 += np.einsum(o_prev, [0,1], dg3_dw2, [0,2,3], range(4))
+	do_dw2 = np.einsum(do_dw2 + do_content_dw2, range(4), g3, [0,3], range(4))
+	do_dw2 += np.einsum(o_prev + o_content, [0,1], dg3_dw2, [0,2,3], range(4))
 	
 	# w3:
 	dg3_dw3 = sq_dF(w3, g2, g3)
 	
-	do_dw3 = np.einsum(do_dw3, [0,1,2], g3, [0,3], [0,1,2])
-	do_dw3 += np.einsum(o_prev, [0,1], dg3_dw3, [0,2], [0,1,2])
+	do_dw3 = np.einsum(do_dw3 + do_content_dw3, [0,1,2], g3, [0,3], [0,1,2])
+	do_dw3 += np.einsum(o_prev + o_content, [0,1], dg3_dw3, [0,2], [0,1,2])
 	
 	o_prev = copy.deepcopy(o)
 	
