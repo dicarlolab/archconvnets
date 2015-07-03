4968411ac55ae1ebe77b270a4bf611a930ca603b
Darren Seibert <darren@munctional1.mit.edu>
2015-07-03 15:46:20 -0400

    m

--- archconvnets/unsupervised/ntm/bp_fd_ntm_time2.py
+++ archconvnets/unsupervised/ntm/bp_fd_ntm_time2.py
@@ -33,27 +33,25 @@ ww = np.random.normal(size=(C, M, n_in)) * SCALE
 
 add_out = np.random.normal(size=(C, mem_length)) * SCALE
 
-shift_out = np.random.normal(size=(C, n_shifts)) * SCALE
+shift_out = np.random.normal(size=(C, n_shifts))
 
 x = np.random.normal(size=(n_in,1))
 x2 = np.random.normal(size=(n_in,1))
 
-do_dw3i = np.zeros((C,M,C,n2))
+do_dw3i = np.zeros((C,M,n2))
 do_dw2i = np.zeros((C,M,n2,n1))
 do_dw1i = np.zeros((C,M,n1,n_in))
-
-dmem_dwr = np.zeros((C, M, mem_length, n_in))
 
 do_content_dw3 = np.zeros_like(do_dw3i)
 do_content_dw2 = np.zeros_like(do_dw2i)
 do_content_dw1 = np.zeros_like(do_dw1i)
 
-
 def read_from_mem_dw_nsum(mem):
 	temp = np.zeros((o_previ.shape[0], mem.shape[1], o_previ.shape[0], o_previ.shape[1]))
 	for i in range(o_previ.shape[0]):
-		for l in range(mem.shape[1]):
-			temp[i,l, i,:] += mem[:,l]
+		for k in range(o_previ.shape[1]):
+			for l in range(mem.shape[1]):
+				temp[i,l, i,k] += mem[k,l]
 	return temp
 
 ##################
@@ -125,7 +123,7 @@ def linear_2d_F(ww,x):
 	return np.squeeze(np.dot(ww,x))
 
 def f(y):
-	w1[i_ind,j_ind] = y
+	w3[i_ind,j_ind] = y
 	
 	o_prev = copy.deepcopy(o_previ)
 	mem_prev = copy.deepcopy(mem_previ)
@@ -166,7 +164,7 @@ def f(y):
 
 
 def g(y):
-	w1[i_ind,j_ind] = y
+	w3[i_ind,j_ind] = y
 	
 	do_dw3 = copy.deepcopy(do_dw3i)
 	do_dw2 = copy.deepcopy(do_dw2i)
@@ -187,8 +185,6 @@ def g(y):
 	gw = linear_2d_F(ww,x)
 	mem = mem_prev + add_mem(gw, add_out)
 	
-	dadd_mem_dgw = add_mem_dgw(add_out)
-	#dgw_dww = 
 	
 	#########
 	dread_mem_do = read_from_mem_dw_nsum(mem_prev)
@@ -200,10 +196,10 @@ def g(y):
 	dg3_dw3 = sq_dF(w3, g2, g3)
 	dg3_dg2 = sq_dlayer_in_nsum(w3, g2)
 	
-	do_in_dw3 = np.einsum(do_dw3 + do_content_dw3, range(4), g3, [0,3], range(4))
-	do_in_dw3 += np.einsum(o_prev + o_content, [0,1], dg3_dw3, [0,2], [0,1,2])[:,:,np.newaxis]#np.einsum(o_prev + o_content, [0,1], dg3_dw3, [0,2], [0,1,2])
+	do_in_dw3 = np.einsum(do_dw3 + do_content_dw3, [0,1,2], g3, [0,3], [0,1,2])
+	do_in_dw3 += np.einsum(o_prev + o_content, [0,1], dg3_dw3, [0,2], [0,1,2])
 	
-	do_dw3 = np.einsum(do_in_dw3, [0,1,2,5], do_do_in, [3,4,0,1], [3,4,2,5])
+	do_dw3 = np.einsum(do_in_dw3, [0,1,2], do_do_in, [3,4,0,1], [3,4,2])
 	
 	# w2:
 	dg2_dw2 = sq_dF(w2, g1, g2)
@@ -214,7 +210,7 @@ def g(y):
 	do_in_dw2 = np.einsum(do_dw2 + do_content_dw2, range(4), g3, [0,3], range(4))
 	do_in_dw2 += np.einsum(o_prev + o_content, [0,1], dg3_dw2, [0,2,3], range(4))
 	
-	do_dw2 = np.einsum(do_in_dw2, [0,1,2,5], do_do_in, [3,4,0,1], [3,4,2,5])
+	do_dw2 = np.einsum(do_do_in, range(4), do_in_dw2, [2,3,4,5], [0,1,4,5])
 	
 	# w1:
 	dg1_dw1 = sq_dF(w1, x, g1)
@@ -225,10 +221,12 @@ def g(y):
 	do_in_dw1 = np.einsum(do_dw1 + do_content_dw1, range(4), g3, [0,4], range(4))
 	do_in_dw1 += np.einsum(o_prev + o_content, [0,1], dg3_dw1, [0,4,2,3], range(4))
 	
-	do_dw1 = np.einsum(do_in_dw1, [0,1,2,5], do_do_in, [3,4,0,1], [3,4,2,5])
+	do_dw1 = np.einsum(do_do_in, range(4), do_in_dw1, [2,3,4,5], [0,1,4,5])
 	
 	o_prev = copy.deepcopy(o)
 	mem_prev = copy.deepcopy(mem)
+	
+	###
 	
 	###
 	g1 = sq_F(w1,x2)
@@ -253,11 +251,10 @@ def g(y):
 	dg3_dw3 = sq_dF(w3, g2, g3)
 	dg3_dg2 = sq_dlayer_in_nsum(w3, g2)
 	
-	do_in_dw3 = np.einsum(do_dw3 + do_content_dw3, range(4), g3, [0,3], range(4))
-	do_in_dw3 += np.einsum(o_prev + o_content, [0,1], dg3_dw3, [0,2], [0,1,2])[:,:,np.newaxis]
+	do_in_dw3 = np.einsum(do_dw3 + do_content_dw3, [0,1,2], g3, [0,3], [0,1,2])
+	do_in_dw3 += np.einsum(o_prev + o_content, [0,1], dg3_dw3, [0,2], [0,1,2])
 	
-	#do_dw3 = np.einsum(do_in_dw3, [0,1,2], do_do_in, [3,4,0,1], [3,4,2])
-	do_dw3 = np.einsum(do_in_dw3, [0,1,2,5], do_do_in, [3,4,0,1], [3,4,2,5])
+	do_dw3 = np.einsum(do_in_dw3, [0,1,2], do_do_in, [3,4,0,1], [3,4,2])
 	
 	# w2:
 	dg2_dw2 = sq_dF(w2, g1, g2)
@@ -268,7 +265,7 @@ def g(y):
 	do_in_dw2 = np.einsum(do_dw2 + do_content_dw2, range(4), g3, [0,3], range(4))
 	do_in_dw2 += np.einsum(o_prev + o_content, [0,1], dg3_dw2, [0,2,3], range(4))
 	
-	do_dw2 = np.einsum(do_in_dw2, [0,1,2,5], do_do_in, [3,4,0,1], [3,4,2,5])
+	do_dw2 = np.einsum(do_do_in, range(4), do_in_dw2, [2,3,4,5], [0,1,4,5])
 	
 	# w1:
 	dg1_dw1 = sq_dF(w1, x2, g1)
@@ -279,7 +276,7 @@ def g(y):
 	do_in_dw1 = np.einsum(do_dw1 + do_content_dw1, range(4), g3, [0,4], range(4))
 	do_in_dw1 += np.einsum(o_prev + o_content, [0,1], dg3_dw1, [0,4,2,3], range(4))
 	
-	do_dw1 = np.einsum(do_in_dw1, [0,1,2,5], do_do_in, [3,4,0,1], [3,4,2,5])
+	do_dw1 = np.einsum(do_do_in, range(4), do_in_dw1, [2,3,4,5], [0,1,4,5])
 	
 	o_prev = copy.deepcopy(o)
 	mem_prev = copy.deepcopy(mem)
@@ -290,9 +287,9 @@ def g(y):
 	
 	dw1 = np.einsum(derr_do, range(4), do_dw1,  [2,3,4,5], [4,5])
 	dw2 = np.einsum(derr_do, range(4), do_dw2,  [2,3,4,5], [4,5])
-	dw3 = np.einsum(derr_do, range(4), do_dw3,  [2,3,4,5],   [4,5])
+	dw3 = np.einsum(derr_do, range(4), do_dw3,  [2,3,4],   [0,4])
 	
-	return dw1[i_ind,j_ind]
+	return dw3[i_ind,j_ind]
 	
 	
 np.random.seed(np.int64(time.time()))
@@ -303,7 +300,7 @@ N_SAMPLES = 25
 ratios = np.zeros(N_SAMPLES)
 for sample in range(N_SAMPLES):
 
-	ref = w1
+	ref = w3
 	i_ind = np.random.randint(ref.shape[0])
 	j_ind = np.random.randint(ref.shape[1])
 	y = -1e0*ref[i_ind,j_ind]; gt = g(y); gtx = scipy.optimize.approx_fprime(np.ones(1)*y, f, eps)
