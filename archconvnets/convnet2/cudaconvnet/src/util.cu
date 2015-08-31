713220a107665f4fb5c19bf5a3a702d67409b843
hyolee <hyo@mit.edu>
2015-08-31 12:45:43 -0400

    Respect the list of CPUs allowed by cgroups (for Openmind)

--- archconvnets/convnet2/cudaconvnet/src/util.cu
+++ archconvnets/convnet2/cudaconvnet/src/util.cu
@@ -177,7 +177,7 @@ std::vector<int>& getDeviceCPUs(int deviceID) {
     if (deviceCPUs.count(deviceID) == 0 && deviceID >= 0) {
         struct cudaDeviceProp props;
         checkCudaErrors(cudaGetDeviceProperties(&props, deviceID));
-        char pciString[13];
+/*        char pciString[13];
 
         sprintf(pciString, "%04x", props.pciDomainID);
         pciString[4] = ':';
@@ -206,7 +206,42 @@ std::vector<int>& getDeviceCPUs(int deviceID) {
         } else {
             printf("Unable to open %s\n", path.c_str());
         }
+*/
+
+        // Find Cpus_allowed_list by cgroups
+        //printf("print pid: %d\n", getpid());
+        char pid[8];
+        sprintf(pid, "%d", getpid());
+        std::string path_pid = std::string("/proc/") + std::string(pid) + "/status";
+        //printf("print path: %s\n", path_pid.c_str());
+        ifstream file(path_pid.c_str());
+
+        if (file.is_open()) {
+            std::string line;
+            //printf("File is opened\n");
+            while (getline(file, line)) {
+                //printf("line %s\n", line.c_str());
+                if (line.find("Cpus_allowed_list") == std::string::npos)
+                        continue;
+                std::istringstream found(line);
+                std::string list;
+                while (getline(found, list, ':')) {
+                        //printf("cpu list: %s\n", list.c_str());
+
+                        std::istringstream iss(list);
+                        std::string cpu;
+                        while (getline(iss, cpu, ',')) {
+                                deviceCPUs[deviceID].push_back(atoi(cpu.c_str()));
+                                //printf("cpu allowed: %d\n", atoi(cpu.c_str()));
+                        }
+                }
+            }
+            file.close();
+        } else {
+            printf("Unable to open %s\n", path_pid.c_str());
+        }
     }
+
     vector<int>& ret = deviceCPUs[deviceID];
     deviceCPULock.release();
     return ret;
