513eec8b09f70497655706b8239550bbaf4a3639
unknown <darren@mit.edu>
2015-09-25 19:27:24 -0400

    cleanup

--- archconvnets/unsupervised/ntm/gradient_tests/bp_fd_test_sharpen.py
+++ archconvnets/unsupervised/ntm/gradient_tests/bp_fd_test_sharpen.py
@@ -8,51 +8,93 @@ import scipy
 from ntm_gradients import *
 from init_vars import *
 
-w = np.abs(np.random.normal(size=(8,1)))
-inputs = np.abs(np.random.normal(size=(8, 15)))
+gamma = np.abs(np.random.normal(size=(8,1)))
+w = np.abs(np.random.normal(size=(8, 15)))
 
 t = np.random.normal(size=(8, 15))
 
-def f(y):
-	#inputs[i_ind,j_ind] = y
-	w[i_ind] = y
+def dwg_dg(w, gamma):
+	n = w.shape[0]
+	g = np.zeros(np.concatenate((w.shape, gamma.shape)))
 	
-	o = sharpen(inputs, w)
+	wg = w ** gamma
+	ln_w_wg = np.log(w)*wg
+	wg_sum = wg.sum(1)[:,np.newaxis]
+	ln_w_wg_sum = ln_w_wg.sum(1)[:,np.newaxis]
+	
+	t = (ln_w_wg * wg_sum - wg * ln_w_wg_sum) / (wg_sum ** 2)
+	
+	g[range(n),:,range(n)] = t[:,:,np.newaxis]
+	
+	return g
+	
+def dwg_dw(w, gamma):
+	n = w.shape[0]
+	g = np.zeros(np.concatenate((w.shape, w.shape)))
+	
+	wg = w ** gamma
+	wg_sum = wg.sum(1)[:,np.newaxis]
+	wg_sum2 = wg_sum ** 2
+	g_wgm1 = gamma * (w ** (gamma-1))
+	
+	t = (g_wgm1 / wg_sum2) * (wg_sum - wg)
+	
+	for i in range(w.shape[0]):
+		g[i,:,i,:] = t[i]
+	
+	for j in range(w.shape[1]):
+		for b in range(w.shape[1]):
+			if b != j:
+				g[range(n),j,range(n),b] = -g_wgm1[:,b] * wg[:,j] / np.squeeze(wg_sum2)
+	
+	return g
+
+def f(y):
+	w[i_ind,j_ind] = y
+	#gamma[i_ind] = y
+	
+	numer = (w ** gamma)
+	denom = (w ** gamma).sum(1)[:,np.newaxis]
+	o = numer / denom
 	
 	return ((o - t)**2).sum()
 
 
 def g(y):
-	#inputs[i_ind,j_ind] = y
-	w[i_ind] = y
+	w[i_ind,j_ind] = y
+	#gamma[i_ind] = y
 	
-	o = sharpen(w, inputs)
+	numer = (w ** gamma)
+	denom = (w ** gamma).sum(1)[:,np.newaxis]
+	o = numer / denom
 	
 	##
 	derr_do = sq_points_dinput(o - t)
 	
-	do_dw = sharpen_dgamma_out_nsum(inputs, w)
+	do_dg = dwg_dg(w, gamma)
+	do_dw = dwg_dw(w, gamma)
 	
+	derr_dg = mult_partials_collapse(derr_do, do_dg, o)
 	derr_dw = mult_partials_collapse(derr_do, do_dw, o)
 	
-	#print do_dw.shape, derr_dw.shape, o.shape
 	##
 	
-	return derr_dw[i_ind]
-	#return derr_dinputs[i_ind,j_ind]
+	#return derr_dg[i_ind]
+	return derr_dw[i_ind,j_ind]
 
 	
 np.random.seed(np.int64(time.time()))
-eps = np.sqrt(np.finfo(np.float).eps)*1e7
+eps = np.sqrt(np.finfo(np.float).eps)*1e1
 
 N_SAMPLES = 25
 ratios = np.zeros(N_SAMPLES)
 for sample in range(N_SAMPLES):
+	#ref = gamma
 	ref = w
 	i_ind = np.random.randint(ref.shape[0])
-	#j_ind = np.random.randint(ref.shape[1])
-	#y = .2+ref[i_ind,j_ind]; gt = g(y); gtx = scipy.optimize.approx_fprime(np.ones(1)*y, f, eps)
-	y = .1 + ref[i_ind]; gt = g(y); gtx = scipy.optimize.approx_fprime(np.ones(1)*y, f, eps)[0]
+	j_ind = np.random.randint(ref.shape[1])
+	y = .2+ref[i_ind,j_ind]; gt = g(y); gtx = scipy.optimize.approx_fprime(np.ones(1)*y, f, eps)
+	#y = .2+ref[i_ind]; gt = g(y); gtx = scipy.optimize.approx_fprime(np.ones(1)*y, f, eps)
 	
 	if gtx == 0:
 		ratios[sample] = 1
