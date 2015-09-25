8f414029a89266d45f92f3a68362d779e97d4479
unknown <darren@mit.edu>
2015-09-25 18:20:13 -0400

    working sharpen gradient wrt w

--- archconvnets/unsupervised/ntm/gradient_tests/bp_fd_test_sharpen_sum_comb_dbg2.py
+++ archconvnets/unsupervised/ntm/gradient_tests/bp_fd_test_sharpen_sum_comb_dbg2.py
@@ -17,23 +17,37 @@ def dwg_dg(w, gamma):
 	g = np.zeros(np.concatenate((w.shape, gamma.shape)))
 	
 	wg = w ** gamma
-	
 	ln_w_wg = np.log(w)*wg
-	
 	wg_sum = wg.sum(1)[:,np.newaxis]
 	ln_w_wg_sum = ln_w_wg.sum(1)[:,np.newaxis]
-	
-	print wg.shape, ln_w_wg.shape, wg_sum.shape, ln_w_wg_sum.shape
 	
 	for i in range(w.shape[0]):
 		for j in range(w.shape[1]):
 			g[i,j,i] = (ln_w_wg[i,j] * wg_sum[i] - wg[i,j] * ln_w_wg_sum[i]) / (wg_sum[i]**2)
 	
 	return g
+	
+def dwg_dw(w, gamma):
+	g = np.zeros(np.concatenate((w.shape, w.shape)))
+	
+	wg = w ** gamma
+	wg_sum = wg.sum(1)[:,np.newaxis]
+	wg_sum2 = wg_sum ** 2
+	g_wgm1 = gamma * (w ** (gamma-1))
+	
+	for i in range(w.shape[0]):
+		for j in range(w.shape[1]):
+			g[i,j,i,j] = (g_wgm1[i,j] / wg_sum2[i]) * (wg_sum[i] - wg[i,j])
+			
+			for b in range(w.shape[1]):
+				if b != j:
+					g[i,j,i,b] = -g_wgm1[i,b] * wg[i,j] / wg_sum2[i]
+	
+	return g
 
 def f(y):
-	#w[i_ind,j_ind] = y
-	gamma[i_ind] = y
+	w[i_ind,j_ind] = y
+	#gamma[i_ind] = y
 	
 	numer = (w ** gamma)
 	denom = (w ** gamma).sum(1)[:,np.newaxis]
@@ -43,8 +57,8 @@ def f(y):
 
 
 def g(y):
-	#w[i_ind,j_ind] = y
-	gamma[i_ind] = y
+	w[i_ind,j_ind] = y
+	#gamma[i_ind] = y
 	
 	numer = (w ** gamma)
 	denom = (w ** gamma).sum(1)[:,np.newaxis]
@@ -54,13 +68,15 @@ def g(y):
 	derr_do = sq_points_dinput(o - t)
 	
 	do_dg = dwg_dg(w, gamma)
+	do_dw = dwg_dw(w, gamma)
 	
 	derr_dg = mult_partials_collapse(derr_do, do_dg, o)
+	derr_dw = mult_partials_collapse(derr_do, do_dw, o)
 	
 	##
 	
-	return derr_dg[i_ind]
-	#return derr_dw[i_ind,j_ind]
+	#return derr_dg[i_ind]
+	return derr_dw[i_ind,j_ind]
 
 	
 np.random.seed(np.int64(time.time()))
@@ -71,9 +87,9 @@ ratios = np.zeros(N_SAMPLES)
 for sample in range(N_SAMPLES):
 	ref = gamma
 	i_ind = np.random.randint(ref.shape[0])
-	#j_ind = np.random.randint(ref.shape[1])
-	#y = .2+ref[i_ind,j_ind]; gt = g(y); gtx = scipy.optimize.approx_fprime(np.ones(1)*y, f, eps)
-	y = .2+ref[i_ind]; gt = g(y); gtx = scipy.optimize.approx_fprime(np.ones(1)*y, f, eps)
+	j_ind = np.random.randint(ref.shape[1])
+	y = .2+ref[i_ind,j_ind]; gt = g(y); gtx = scipy.optimize.approx_fprime(np.ones(1)*y, f, eps)
+	#y = .2+ref[i_ind]; gt = g(y); gtx = scipy.optimize.approx_fprime(np.ones(1)*y, f, eps)
 	
 	if gtx == 0:
 		ratios[sample] = 1
