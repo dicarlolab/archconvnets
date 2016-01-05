b999a8a2b99f7f80cbb643793336c55a21bde181
Darren Seibert <darren@mit.edu>
2016-01-04 19:36:10 -0500

    m

--- archconvnets/unsupervised/ntm2/bp_fd_ntm.py
+++ archconvnets/unsupervised/ntm2/bp_fd_ntm.py
@@ -11,18 +11,18 @@ mem_shape = (N_MEM_SLOTS, M_LENGTH)
 
 free_all_buffers()
 
-#############
+############# init layers
 LAYERS = []
 
 FW_IND = add_linear_F_layer(LAYERS, 'FW', N_MEM_SLOTS, (8, M_LENGTH))
 S1_IND = add_sigmoid_layer(LAYERS, 'S1')
-FS_IND = add_sharpen_layer(LAYERS, 'FS', ['S1', random_function_1])
+#FS_IND = add_sharpen_layer(LAYERS, 'FS', ['S1', random_function_1])
 MEM_IND = add_add_layer(LAYERS, 'MEM', ['FS', 'MEM'])
 FM_IND = add_focus_keys_layer(LAYERS, 'FM', ['MEM', random_function])
 add_linear_F_layer(LAYERS, 'F3', 25)
 add_sum_layer(LAYERS, 'SUM')
 
-################
+################ init weights and inputs
 
 WEIGHTS = init_weights(LAYERS)
 xt = random_function(np.concatenate(((N_FRAMES,), LAYERS[FW_IND]['in_shape'][1])))
@@ -31,11 +31,9 @@ mem_init = random_function(LAYERS[MEM_IND]['out_shape'])
 
 DERIV_TOP = init_buffer(np.ones((1,1), dtype='single'))
 
-################
+################ which gradient to test
 gradient_layer = FW_IND
 gradient_arg = 0
-assert isinstance(LAYERS[gradient_layer]['in_source'][gradient_arg], int) != True, 'derivative of intermediate layer'
-ref = return_buffer(WEIGHTS[gradient_layer][gradient_arg])
 
 def f(y):
 	OUTPUT = None; OUTPUT_PREV = [None] * len(LAYERS)
@@ -45,9 +43,7 @@ def f(y):
 	set_buffer(Wy.reshape(weights_shape), WEIGHTS[gradient_layer][gradient_arg])
 	
 	for frame in range(N_FRAMES):
-		set_buffer(xt[frame], WEIGHTS[FW_IND][1])  # inputs
-		#set_buffer(ft[frame], WEIGHTS[FM_IND][1])  # inputs
-		
+		set_buffer(xt[frame], WEIGHTS[FW_IND][1])  # inputs	
 		OUTPUT = forward_network(LAYERS, WEIGHTS, OUTPUT, OUTPUT_PREV)
 		OUTPUT_PREV = copy_list(OUTPUT, OUTPUT_PREV)
 	
@@ -67,10 +63,7 @@ def g(y):
 	PARTIALS_PREV = init_partials(LAYERS)
 	for frame in range(N_FRAMES):
 		set_buffer(xt[frame], WEIGHTS[FW_IND][1])  # inputs
-		#set_buffer(ft[frame], WEIGHTS[FM_IND][1])  # inputs
-		
 		OUTPUT = forward_network(LAYERS, WEIGHTS, OUTPUT, OUTPUT_PREV)
-		
 		LOCAL_DERIVS = local_derivs(LAYERS, WEIGHTS, OUTPUT, OUTPUT_PREV, LOCAL_DERIVS)
 		WEIGHT_DERIVS = reverse_network(DERIV_TOP, len(LAYERS)-1, LAYERS, LOCAL_DERIVS, PARTIALS_PREV, WEIGHT_DERIVS)
 		
@@ -89,6 +82,8 @@ def g(y):
 	free_list(OUTPUT_PREV)
 	return z
 
+assert isinstance(LAYERS[gradient_layer]['in_source'][gradient_arg], int) != True, 'derivative of intermediate layer'
+ref = return_buffer(WEIGHTS[gradient_layer][gradient_arg])
 np.random.seed(np.int64(time.time()))
 eps = np.sqrt(np.finfo(np.float).eps)*1e6
 
