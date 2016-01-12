bugfix involving mem gradient wrt weights under multiple branches

archconvnets/unsupervised/ntm2/bp_fd_ntm.py
@@ -3,7 +3,7 @@ import time
 import scipy.optimize
 from ntm_core import *
 
-N_FRAMES = 10
+N_FRAMES = 3
 N_CONTROLLERS = 16
 N_MEM_SLOTS = 6
 M_LENGTH = 8
@@ -14,6 +14,15 @@ mem_shape = (N_MEM_SLOTS, M_LENGTH)
 free_all_buffers()
 
 ############# init layers
+KEY_IND = {}; CONTENT_IND = {}; 
+BETA_IND = {}; CONTENT_FOCUSED_IND = {}; 
+CONTENT_SM_IND = {}; IN_GATE_PRE_IND = {}; 
+IN_GATE_IND = {}; IN_PRE_IND = {}; 
+IN_IND = {}; SHIFT_PRE_IND = {}; 
+SHIFT_IND = {}; SHIFTED_IND = {}; 
+GAMMA_PRE_IND = {}; GAMMA_IND = {}; 
+GAMMA_PRE_IND = {}; F_IND = {}; 
+
 LAYERS = []
 
 N_F1 = 12
@@ -27,51 +36,18 @@ for init in [0,1]:
 	F2_IND = add_linear_F_layer(LAYERS, 'F2', N_F2, init=init)
 	F3_IND = add_linear_F_layer(LAYERS, HEAD_INPUT, N_F3, init=init)
 
-	###### read
-	# content
-	R_KEY_IND = add_linear_F_layer(LAYERS, 'R_KEY', (N_CONTROLLERS, M_LENGTH), HEAD_INPUT, init=init)
-	R_CONTENT_IND = add_cosine_sim_layer(LAYERS, 'R_CONTENT', ['R_KEY', 'MEM-'], mem_shape, init=init)
-	R_BETA_IND = add_linear_F_layer(LAYERS, 'R_BETA', N_CONTROLLERS, HEAD_INPUT, init=init)
-	R_CONTENT_FOCUSED_IND = add_focus_keys_layer(LAYERS, 'R_CONTENT_FOCUSED', ['R_CONTENT', 'R_BETA'], init=init)
-	R_CONTENT_SM_IND = add_softmax_layer(LAYERS, 'R_CONTENT_SM', init=init)
+	MEM_T_IND = add_linear_F_layer(LAYERS, 'MEM_T', (N_MEM_SLOTS, M_LENGTH), HEAD_INPUT, squeeze=True, init=init)
+
 	
-	# interpolate
-	R_IN_GATE_PRE_IND = add_linear_F_layer(LAYERS, 'R_IN_GATE_PRE', N_CONTROLLERS, HEAD_INPUT, init=init)
-	R_IN_GATE_IND = add_sigmoid_layer(LAYERS, 'R_IN_GATE', init=init)
+	ERASE_PRE_IND = add_linear_F_layer(LAYERS, 'ERASE_PRE', (N_CONTROLLERS, M_LENGTH), HEAD_INPUT, squeeze=True, init=init)
+	add_linear_F_layer(LAYERS, 'W_CONTENT2', (N_CONTROLLERS, N_MEM_SLOTS), HEAD_INPUT, squeeze=True, init=init)
+	ERASE_HEAD_IND = add_dotT_layer(LAYERS, 'ERASE_HEAD', ['W_CONTENT2', 'ERASE_PRE'], init=init)
 	
-	##
-	R_T_F_IND = add_linear_F_layer(LAYERS, 'R_T_F', N_CONTROLLERS, (4, N_MEM_SLOTS), init=init)
-	
-	R_IN_PRE_IND = add_interpolate_layer(LAYERS, 'R_IN_PRE', ['R_IN_GATE', 'R_CONTENT_SM', 'R_F-'], init=init)
-	R_IN_IND = add_softmax_layer(LAYERS, 'R_IN', init=init)
-	
-	# shift
-	R_SHIFT_PRE_IND = add_linear_F_layer(LAYERS, 'R_SHIFT_PRE', (N_CONTROLLERS, N_SHIFTS), HEAD_INPUT, init=init)
-	R_SHIFT_IND = add_softmax_layer(LAYERS, 'R_SHIFT', init=init)
-	R_SHIFTED_IND = add_shift_w_layer(LAYERS, 'R_SHIFTED', ['R_SHIFT', 'R_IN'], init=init)
-	
-	# sharpen
-	R_GAMMA_PRE_IND = add_linear_F_layer(LAYERS, 'R_GAMMA_PRE', N_CONTROLLERS, HEAD_INPUT, init=init)
-	R_GAMMA_IND = add_relu_layer(LAYERS, 'R_GAMMA', init=init)
-	R_F_IND = add_sharpen_layer(LAYERS, 'R_F', ['R_SHIFTED', 'R_GAMMA'], init=init)
-	
-	##
-	R_T_IND = add_linear_F_layer(LAYERS, 'R_T', N_MEM_SLOTS, (3, M_LENGTH), init=init)
-	
-	MEM_IND = add_add_layer(LAYERS, 'MEM', ['R_T', 'MEM-'], -1, init=init)
+	MEM_IND = add_add_layer(LAYERS, 'MEM', ['ERASE_HEAD', 'MEM-'], -1, init=init)
+	#MEM_IND = add_add_layer(LAYERS, 'MEM', ['MEM_T', 'MEM-'], -1, init=init)
 	SQ_IND = add_sq_points_layer(LAYERS, 'SQ', init=init)
 	add_sum_layer(LAYERS, 'SUM', init=init)
 
-'''for init in [0,1]:
-	F1_IND = add_linear_F_layer(LAYERS, 'F1', N_CONTROLLERS, (3,M_LENGTH), init=init)
-	F2_IND = add_linear_F_layer(LAYERS, 'F2', N_CONTROLLERS, (3,M_LENGTH), init=init)
-	RCONTENT_SUM_IND = add_add_layer(LAYERS, 'RCONTENT_SUM', ['F1', 'MEM-'], init=init)
-	RCONTENT_SUM2_IND = add_add_layer(LAYERS, 'RCONTENT_SUM2', ['F2', 'MEM2-'], init=init)
-
-	MEM_IND = add_add_layer(LAYERS, 'MEM', ['RCONTENT_SUM', 'MEM-'], init=init)
-	MEM2_IND = add_add_layer(LAYERS, 'MEM2', ['RCONTENT_SUM2', 'MEM2-'], init=init)
-	MEM3_IND = add_add_layer(LAYERS, 'MEM3', ['MEM-', 'MEM2'], init=init)
-	add_sum_layer(LAYERS, 'SUM', init=init)'''
 
 check_network(LAYERS)
 
@@ -79,22 +55,18 @@ check_network(LAYERS)
 
 WEIGHTS = init_weights(LAYERS)
 x1t = random_function(np.concatenate(((N_FRAMES,), LAYERS[F1_IND]['in_shape'][1])))
-x2t = random_function(np.concatenate(((N_FRAMES,), LAYERS[R_T_IND]['in_shape'][1])))
-x3t = random_function(np.concatenate(((N_FRAMES,), LAYERS[R_T_F_IND]['in_shape'][1])))
 mem_init = random_function(LAYERS[MEM_IND]['out_shape'])
-R_F_init = random_function(LAYERS[R_F_IND]['out_shape'])
-#mem2_init = random_function(LAYERS[MEM2_IND]['out_shape'])
 
 DERIV_TOP = init_buffer(np.ones((1,1), dtype='single'))
 
 ################ which gradient to test
-gradient_layer = R_T_IND
+gradient_layer = F1_IND
 gradient_arg = 0
 
 def f(y):
 	OUTPUT = None; OUTPUT_PREV = [None] * len(LAYERS)
 	OUTPUT_PREV[MEM_IND] = init_buffer(mem_init)
-	OUTPUT_PREV[R_F_IND] = init_buffer(R_F_init)
+	#OUTPUT_PREV[F_IND['R']] = init_buffer(R_F_init)
 	#OUTPUT_PREV[MEM2_IND] = init_buffer(mem2_init)
 	Wy = return_buffer(WEIGHTS[gradient_layer][gradient_arg])
 	weights_shape = Wy.shape; Wy = Wy.ravel(); Wy[i_ind] = y
@@ -102,8 +74,7 @@ def f(y):
 	
 	for frame in range(N_FRAMES):
 		set_buffer(x1t[frame], WEIGHTS[F1_IND][1])  # inputs
-		set_buffer(x2t[frame], WEIGHTS[R_T_IND][1])  # inputs
-		set_buffer(x3t[frame], WEIGHTS[R_T_F_IND][1])  # inputs
+
 		OUTPUT = forward_network(LAYERS, WEIGHTS, OUTPUT, OUTPUT_PREV)
 		OUTPUT_PREV = copy_list(OUTPUT, OUTPUT_PREV)
 	
@@ -117,7 +88,7 @@ def g(y):
 	OUTPUT_PREV = [None] * len(LAYERS); MEM_WEIGHT_DERIVS = None
 	MEM2_WEIGHT_DERIVS = None; R_F_DERIVS = None
 	OUTPUT_PREV[MEM_IND] = init_buffer(mem_init)
-	OUTPUT_PREV[R_F_IND] = init_buffer(R_F_init)
+	#OUTPUT_PREV[F_IND['R']] = init_buffer(R_F_init)
 	#OUTPUT_PREV[MEM2_IND] = init_buffer(mem2_init)
 	Wy = return_buffer(WEIGHTS[gradient_layer][gradient_arg])
 	weights_shape = Wy.shape; Wy = Wy.ravel(); Wy[i_ind] = y
@@ -126,8 +97,7 @@ def g(y):
 	PARTIALS_PREV = init_partials(LAYERS)
 	for frame in range(N_FRAMES):
 		set_buffer(x1t[frame], WEIGHTS[F1_IND][1])  # inputs
-		set_buffer(x2t[frame], WEIGHTS[R_T_IND][1])  # inputs
-		set_buffer(x3t[frame], WEIGHTS[R_T_F_IND][1])  # inputs
+		
 		OUTPUT = forward_network(LAYERS, WEIGHTS, OUTPUT, OUTPUT_PREV)
 		LOCAL_DERIVS = local_derivs(LAYERS, WEIGHTS, OUTPUT, OUTPUT_PREV, LOCAL_DERIVS)
 		WEIGHT_DERIVS = reverse_network(DERIV_TOP, len(LAYERS)-1, LAYERS, LOCAL_DERIVS, PARTIALS_PREV, WEIGHT_DERIVS)
@@ -154,7 +124,7 @@ def g(y):
 assert isinstance(LAYERS[gradient_layer]['in_source'][gradient_arg], int) != True, 'derivative of intermediate layer'
 ref = return_buffer(WEIGHTS[gradient_layer][gradient_arg])
 np.random.seed(np.int64(time.time()))
-eps = np.sqrt(np.finfo(np.float).eps)*5e3
+eps = np.sqrt(np.finfo(np.float).eps)*1e6
 
 N_SAMPLES = 25
 ratios = np.zeros(N_SAMPLES)

archconvnets/unsupervised/ntm2/ntm_core.py
@@ -49,7 +49,7 @@ def check_network(LAYERS):
 				OUT = L['deriv_F'][arg](args, LAYER_OUT, additional_args=L['additional_deriv_args'][arg])
 			else:
 				OUT = L['deriv_F'][arg](args, LAYER_OUT)
-			assert OUT[1] == expected_shape
+			assert OUT[1] == expected_shape, 'deriv not expected size (layer %s)' % L['name']
 			free_buffer(OUT)
 		free_buffer(LAYER_OUT)
 		
@@ -250,10 +250,18 @@ def init_traverse_to_end(layer_orig, layer_cur, arg, LAYERS, PARTIALS):
 		
 		# input or weights, end:
 		if (isinstance(dest, int) == False) or dest == -1:
-			PARTIALS[layer_orig]['in_source'].append(layer_cur)
-			PARTIALS[layer_orig]['in_arg'].append(arg)
-			OUT = init_buffer(np.zeros(np.concatenate((LAYERS[layer_orig]['out_shape'], LAYERS[layer_cur]['in_shape'][arg])), dtype='single'))
-			PARTIALS[layer_orig]['partial'].append(OUT)
+			# have these inputs already been added?
+			t1 = np.asarray(PARTIALS[layer_orig]['in_source'])
+			t2 = np.asarray(PARTIALS[layer_orig]['in_arg'])
+			inds = np.nonzero((t1 == layer_cur) * (t2 == arg))[0]
+			assert len(inds) <= 1, 'partials have been added more than once'
+			
+			# inputs have not been added, add them:
+			if len(inds) == 0:
+				PARTIALS[layer_orig]['in_source'].append(layer_cur)
+				PARTIALS[layer_orig]['in_arg'].append(arg)
+				OUT = init_buffer(np.zeros(np.concatenate((LAYERS[layer_orig]['out_shape'], LAYERS[layer_cur]['in_shape'][arg])), dtype='single'))
+				PARTIALS[layer_orig]['partial'].append(OUT)
 		
 		# another layer, go back farther through the network:
 		else:
@@ -287,8 +295,16 @@ def copy_traverse_to_end(layer_orig, layer_cur, arg, LAYERS, PARTIALS, MEM_WEIGH
 	if LAYERS[layer_cur]['in_prev'][arg] == False:
 		# end (weighting, input or mem layer input):
 		if (isinstance(dest, int) == False) or dest == -1:
-			buffer = copy_buffer(MEM_WEIGHT_DERIVS[layer_cur][arg])
-			PARTIALS[layer_orig]['partial'].append(buffer)
+			# have these inputs already been added?
+			t1 = np.asarray(PARTIALS[layer_orig]['in_source'])
+			t2 = np.asarray(PARTIALS[layer_orig]['in_arg'])
+			inds = np.nonzero((t1 == layer_cur) * (t2 == arg))[0]
+			assert len(inds) == 1, 'partials have not been added to partials list'
+			
+			# copy partials to mem_weight_derivs
+			# note: there is redundant copying happening if a layer contributes to multiple
+			# branches...this in principle should be checked for to save some time
+			copy_buffer(MEM_WEIGHT_DERIVS[layer_cur][arg], PARTIALS[layer_orig]['partial'][inds[0]])
 		
 		# continue (another layer)
 		else:
@@ -305,8 +321,6 @@ def copy_partials(layer_ind, LAYERS, PARTIALS_PREV, MEM_WEIGHT_DERIVS):
 	
 	for arg in range(N_ARGS):
 		if L['in_prev'][arg] == False:
-			free_list(PARTIALS_PREV[layer_ind]['partial'])
-			PARTIALS_PREV[layer_ind]['partial'] = []
 			PARTIALS_PREV = copy_traverse_to_end(layer_ind, layer_ind, arg, LAYERS, PARTIALS_PREV, MEM_WEIGHT_DERIVS)
 	return PARTIALS_PREV
 

archconvnets/unsupervised/ntm_module2/gradient_functions/add_points.py
@@ -72,18 +72,25 @@ def add_add_layer(LAYERS, name, source, scalar=1, init=0):
 		assert layer_ind is not None, 'layer %s has not already been added' % name
 		
 		in_prev = [None]*2
-		in_prev[0] = False
+		
+		in_prev[0] = source[0][-1] == '-'
+		in_prev[1] = source[1][-1] == '-'
 		
 		source_A = find_layer(LAYERS, source[0])
 		assert source_A is not None, 'could not find layer %s' % source[0]
-		out_shape = LAYERS[source_A]['out_shape']
 		
 		source_B = find_layer(LAYERS, source[1])
+		assert source_B is not None, 'could not find layer %s' % source[1]
+		
 		assert source_A != source_B
-		assert source_B is not None
-		in_prev[1] = source[1][-1] == '-'
-		if source_B < layer_ind:
-			assert out_shape == LAYERS[source_B]['out_shape']
+		
+		if in_prev[0]:
+			out_shape = LAYERS[source_B]['out_shape']
+		else:
+			out_shape = LAYERS[source_A]['out_shape']
+		
+		if np.sum(in_prev) == 0:
+			assert LAYERS[source_B]['out_shape'] == LAYERS[source_A]['out_shape']
 		
 		LAYERS[layer_ind]['forward_F'] = add_points
 		LAYERS[layer_ind]['out_shape'] = out_shape

archconvnets/unsupervised/ntm_module2/gradient_functions/cosine_sim.py
@@ -14,7 +14,7 @@ def cosine_sim(args, OUT_BUFFER=None, gpu_ind=0):
 	if OUT_BUFFER is None:
 		OUT_BUFFER = init_buffer(gpu_ind=gpu_ind)
 	check_buffer(OUT_BUFFER)
-	assert len(MEM[1]) == 2
+	assert (len(MEM[1]) == 2) or ((len(MEM[1]) == 3) and (MEM[1][2] == 1))
 	assert (len(KEYS[1]) == 2) or ((len(KEYS[1]) == 3) and (KEYS[1][2] == 1))
 	assert KEYS[0] != MEM[0]
 	assert OUT_BUFFER[0] != KEYS[0]
@@ -25,11 +25,11 @@ def cosine_sim(args, OUT_BUFFER=None, gpu_ind=0):
 	M = MEM[1][0]
 	
 	if GPU:
-		_ntm_module2.cosine_sim(KEYS[0], KEYS[1], MEM[0], MEM[1], OUT_BUFFER[0], gpu_ind)
+		_ntm_module2.cosine_sim(KEYS[0], KEYS[1][:2], MEM[0], MEM[1][:2], OUT_BUFFER[0], gpu_ind)
 	else:
 		######## CPU
 		keys = return_buffer(KEYS, gpu_ind)[:,:,0]
-		mem = return_buffer(MEM, gpu_ind)
+		mem = return_buffer(MEM, gpu_ind)[:,:,0]
 		numer = np.dot(keys, mem.T)
 		denom = np.einsum(np.sqrt(np.sum(keys**2,1)), [0], np.sqrt(np.sum(mem**2,1)), [1], [0,1])
 		OUT_BUFFER = set_buffer(numer/denom, OUT_BUFFER, gpu_ind)
@@ -46,7 +46,7 @@ def cosine_sim_dmem(args, LAYER_OUT, OUT_BUFFER=None, gpu_ind=0):
 	if OUT_BUFFER is None:
 		OUT_BUFFER = init_buffer(gpu_ind=gpu_ind)
 	check_buffer(OUT_BUFFER)
-	assert len(MEM[1]) == 2
+	assert (len(MEM[1]) == 2) or ((len(MEM[1]) == 3) and (MEM[1][2] == 1))
 	assert (len(KEYS[1]) == 2) or ((len(KEYS[1]) == 3) and (KEYS[1][2] == 1))
 	assert KEYS[0] != MEM[0]
 	assert OUT_BUFFER[0] != KEYS[0]
@@ -57,11 +57,11 @@ def cosine_sim_dmem(args, LAYER_OUT, OUT_BUFFER=None, gpu_ind=0):
 	M = MEM[1][0]
 	
 	if GPU:
-		_ntm_module2.cosine_sim_dmem(KEYS[0], KEYS[1], MEM[0], MEM[1], OUT_BUFFER[0], gpu_ind)
+		_ntm_module2.cosine_sim_dmem(KEYS[0], KEYS[1][:2], MEM[0], MEM[1][:2], OUT_BUFFER[0], gpu_ind)
 	else:
 		########## CPU
 		keys = return_buffer(KEYS, gpu_ind)[:,:,0]
-		mem = return_buffer(MEM, gpu_ind)
+		mem = return_buffer(MEM, gpu_ind)[:,:,0]
 		comb = np.zeros((n_controllers, mem.shape[0], mem.shape[0], mem.shape[1]),dtype='single')
 
 		keys_sq_sum = np.sqrt(np.sum(keys**2, 1))
@@ -79,8 +79,11 @@ def cosine_sim_dmem(args, LAYER_OUT, OUT_BUFFER=None, gpu_ind=0):
 		
 		comb[:,range(mem.shape[0]),range(mem.shape[0])] = keys_denom - temp
 		OUT_BUFFER = set_buffer(comb, OUT_BUFFER, gpu_ind)
-		
-	OUT_BUFFER[1] = (n_controllers, M, M, mem_length)
+	
+	if len(MEM[1]) == 2:
+		OUT_BUFFER[1] = (n_controllers, M, M, mem_length)
+	else:
+		OUT_BUFFER[1] = (n_controllers, M, M, mem_length,1)
 	return OUT_BUFFER
 
 def cosine_sim_dkeys(args, LAYER_OUT, OUT_BUFFER=None, gpu_ind=0):
@@ -92,7 +95,7 @@ def cosine_sim_dkeys(args, LAYER_OUT, OUT_BUFFER=None, gpu_ind=0):
 	if OUT_BUFFER is None:
 		OUT_BUFFER = init_buffer(gpu_ind=gpu_ind)
 	check_buffer(OUT_BUFFER)
-	assert len(MEM[1]) == 2
+	assert (len(MEM[1]) == 2) or ((len(MEM[1]) == 3) and (MEM[1][2] == 1))
 	assert (len(KEYS[1]) == 2) or ((len(KEYS[1]) == 3) and (KEYS[1][2] == 1))
 	assert KEYS[0] != MEM[0]
 	assert OUT_BUFFER[0] != KEYS[0]
@@ -103,11 +106,11 @@ def cosine_sim_dkeys(args, LAYER_OUT, OUT_BUFFER=None, gpu_ind=0):
 	M = MEM[1][0]
 	
 	if GPU:
-		_ntm_module2.cosine_sim_dkeys(KEYS[0], KEYS[1], MEM[0], MEM[1], OUT_BUFFER[0], gpu_ind)
+		_ntm_module2.cosine_sim_dkeys(KEYS[0], KEYS[1][:2], MEM[0], MEM[1][:2], OUT_BUFFER[0], gpu_ind)
 	else:
 		######## CPU
 		keys = return_buffer(KEYS, gpu_ind)[:,:,0]
-		mem = return_buffer(MEM, gpu_ind)
+		mem = return_buffer(MEM, gpu_ind)[:,:,0]
 		comb = np.zeros((n_controllers, mem.shape[0], n_controllers, keys.shape[1]),dtype='single')
 		
 		keys_sq_sum = np.sqrt(np.sum(keys**2, 1))

archconvnets/unsupervised/ntm_module2/gradient_functions/linear_F.py
@@ -7,7 +7,7 @@ from archconvnets.unsupervised.ntm2.ntm_core import *
 def random_function(size):
 	return np.asarray(np.random.random(size) - .5, dtype='single')
 
-def linear_F_dx(args, LAYER_OUT, OUT_BUFFER=None, gpu_ind=0):
+def linear_F_dx(args, LAYER_OUT, OUT_BUFFER=None, additional_args=[True], gpu_ind=0):
 	assert isinstance(gpu_ind,int)
 	F, X = args
 	check_buffer(F)
@@ -39,10 +39,13 @@ def linear_F_dx(args, LAYER_OUT, OUT_BUFFER=None, gpu_ind=0):
 		temp[:,range(n),:,range(n)] = f
 		OUT_BUFFER = set_buffer(temp, OUT_BUFFER, gpu_ind)
 	
-	OUT_BUFFER[1] = tuple(np.concatenate((np.asarray(F[1][:len(F[1])-1]), np.asarray(X[1][1])[np.newaxis], np.asarray(X[1]))))
+	if additional_args[0] and X[1][1] == 1: # squeeze
+		OUT_BUFFER[1] = tuple(np.concatenate((np.asarray(F[1][:len(F[1])-1]), np.asarray(X[1]))))
+	else:
+		OUT_BUFFER[1] = tuple(np.concatenate((np.asarray(F[1][:len(F[1])-1]), np.asarray(X[1][1])[np.newaxis], np.asarray(X[1]))))
 	return OUT_BUFFER
 
-def linear_F_dF(args, LAYER_OUT, OUT_BUFFER=None, gpu_ind=0):
+def linear_F_dF(args, LAYER_OUT, OUT_BUFFER=None, additional_args=[True], gpu_ind=0):
 	assert isinstance(gpu_ind,int)
 	F, X = args
 	check_buffer(F)
@@ -74,13 +77,16 @@ def linear_F_dF(args, LAYER_OUT, OUT_BUFFER=None, gpu_ind=0):
 		temp[range(n),:,range(n)] = x.T
 		OUT_BUFFER = set_buffer(temp, OUT_BUFFER, gpu_ind)
 		
-	OUT_BUFFER[1] = tuple(np.concatenate((np.asarray(F[1][:len(F[1])-1]), np.asarray(X[1][1])[np.newaxis], np.asarray(F[1]))))
+	if additional_args[0] and X[1][1] == 1: # squeeze
+		OUT_BUFFER[1] = tuple(np.concatenate((np.asarray(F[1][:len(F[1])-1]), np.asarray(F[1]))))
+	else:
+		OUT_BUFFER[1] = tuple(np.concatenate((np.asarray(F[1][:len(F[1])-1]), np.asarray(X[1][1])[np.newaxis], np.asarray(F[1]))))
 	#OUT_BUFFER[1] = (F_dim0, X_dim1, F_dim0, X_dim0)
 	return OUT_BUFFER
 	
 linear_F = dot
 
-def add_linear_F_layer(LAYERS, name, n_filters, source=None, random_function=random_function, init=0):
+def add_linear_F_layer(LAYERS, name, n_filters, source=None, squeeze=False, random_function=random_function, init=0):
 	assert isinstance(name, str)
 	if init == 0:
 		assert find_layer(LAYERS, name) is None, 'layer %s has already been added' % name
@@ -116,12 +122,17 @@ def add_linear_F_layer(LAYERS, name, n_filters, source=None, random_function=ran
 			in_shape[0] = tuple(np.concatenate((np.asarray(n_filters), np.asarray(in_shape[1][0])[np.newaxis])))
 			out_shape = tuple(np.concatenate((in_shape[0][:len(in_shape[0])-1], np.asarray(in_shape[1][1])[np.newaxis])))
 		
+		if squeeze and out_shape[-1] == 1:
+			out_shape = out_shape[:len(out_shape)-1]
+		
 		LAYERS[layer_ind]['forward_F'] = linear_F
 		LAYERS[layer_ind]['out_shape'] = out_shape
 		LAYERS[layer_ind]['in_shape'] = in_shape
 		LAYERS[layer_ind]['in_source'] = [random_function, in_source]
 		LAYERS[layer_ind]['deriv_F'] = [linear_F_dF, linear_F_dx]
 		LAYERS[layer_ind]['in_prev'] = [False, False]
+		LAYERS[layer_ind]['additional_forward_args'] = [squeeze]
+		LAYERS[layer_ind]['additional_deriv_args'] = [[squeeze], [squeeze]]
 		
 		return layer_ind
 		

archconvnets/unsupervised/ntm_module2/gradient_functions/sigmoid.py
@@ -12,7 +12,7 @@ def sigmoid(args, OUT_BUFFER=None, gpu_ind=0):
 	assert len(args) == 1
 	LAYER_IN = args[0]
 	check_buffer(LAYER_IN)
-	assert len(LAYER_IN[1]) == 2
+	assert (len(LAYER_IN[1]) == 2) or ((len(LAYER_IN[1]) == 3) and (LAYER_IN[1][2] == 1))
 	
 	if OUT_BUFFER != None:
 		check_buffer(OUT_BUFFER)
@@ -25,7 +25,7 @@ def sigmoid(args, OUT_BUFFER=None, gpu_ind=0):
 		####### CPU
 		layer_in = return_buffer(LAYER_IN,gpu_ind)
 		OUT_BUFFER = set_buffer(1/(1+np.exp(-layer_in)), OUT_BUFFER, gpu_ind)
-	OUT_BUFFER[1] = copy.deepcopy(LAYER_IN[1])
+	OUT_BUFFER[1] = copy.deepcopy(LAYER_IN[1][:2])
 		
 	return OUT_BUFFER
 
@@ -33,7 +33,8 @@ def sigmoid_dlayer_in(args, LAYER_OUT, OUT_BUFFER=None, gpu_ind=0):
 	assert isinstance(gpu_ind,int)
 	assert len(args) == 1
 	check_buffer(args[0])
-	assert len(args[0][1]) == 2
+	LAYER_IN = args[0]
+	assert (len(LAYER_IN[1]) == 2) or ((len(LAYER_IN[1]) == 3) and (LAYER_IN[1][2] == 1))
 	check_buffer(LAYER_OUT)
 	if OUT_BUFFER is None:
 		OUT_BUFFER = init_buffer(gpu_ind=gpu_ind)
@@ -52,7 +53,10 @@ def sigmoid_dlayer_in(args, LAYER_OUT, OUT_BUFFER=None, gpu_ind=0):
 				t[i,j,i,j] = d[i,j]
 		OUT_BUFFER = set_buffer(t, OUT_BUFFER, gpu_ind)
 	
-	OUT_BUFFER[1] = (dim1,dim2,dim1,dim2)
+	if len(LAYER_IN[1]) == 3:
+		OUT_BUFFER[1] = (dim1,dim2,dim1,dim2,1)
+	else:
+		OUT_BUFFER[1] = (dim1,dim2,dim1,dim2)
 	return OUT_BUFFER
 
 
@@ -84,8 +88,10 @@ def add_sigmoid_layer(LAYERS, name, source=None, init=0):
 		else:
 			assert False, 'unknown source input'
 		
+		assert (len(in_shape[0]) == 2) or ((len(in_shape[0]) == 3) and (in_shape[0][2] == 1))
+		
 		LAYERS[layer_ind]['forward_F'] = sigmoid
-		LAYERS[layer_ind]['out_shape'] = in_shape[0]
+		LAYERS[layer_ind]['out_shape'] = in_shape[0][:2]
 		LAYERS[layer_ind]['in_shape'] = in_shape
 		LAYERS[layer_ind]['in_source'] = [in_source]
 		LAYERS[layer_ind]['deriv_F'] = [sigmoid_dlayer_in]

archconvnets/unsupervised/ntm_module2/ntm_module2.py
@@ -85,6 +85,18 @@ def free_list(LIST):
 			else:
 				free_buffer(LIST[layer_ind])
 
+def zero_list(LIST):
+	for layer_ind in range(len(LIST)):
+		if LIST[layer_ind] is not None:
+			# list of list, ex. partials
+			if isinstance(LIST[layer_ind][0], list):
+				for arg in range(len(LIST[layer_ind])):
+					zero_buffer(LIST[layer_ind][arg])
+			# outputs
+			else:
+				zero_buffer(LIST[layer_ind])
+				
+				
 def set_buffer(DATA, DATA_G, gpu_ind=0, warn=True):
 	assert DATA.dtype == np.dtype('float32')
 	if not DATA.flags.contiguous and warn:
@@ -149,6 +161,7 @@ def return_buffer(BUFFER, warn=1, gpu_ind=0):
 			return 0
 
 # a += b * scalar
+# when OUT_BUFFER=None, store results in "a"
 def point_wise_add(args, OUT_BUFFER=None, scalar=1, gpu_ind=0):
 	assert isinstance(gpu_ind,int)
 	A, B = args
@@ -172,7 +185,8 @@ def point_wise_add(args, OUT_BUFFER=None, scalar=1, gpu_ind=0):
 	OUT_BUFFER[1] = copy.deepcopy(B[1])
 	return OUT_BUFFER
 
-def dot(args, OUT_BUFFER=None, increment=0, gpu_ind=0):
+# additional_args: Squeeze output or not
+def dot(args, OUT_BUFFER=None, increment=0, additional_args=[True], gpu_ind=0):
 	assert isinstance(gpu_ind,int)
 	BUFFER1, BUFFER2 = args
 	check_buffer(BUFFER1)
@@ -201,8 +215,10 @@ def dot(args, OUT_BUFFER=None, increment=0, gpu_ind=0):
 		x = return_buffer(BUFFER2, gpu_ind)
 		temp = np.asarray(np.dot(F,x),dtype='single') # [n1, 1]
 		OUT_BUFFER = set_buffer(temp, OUT_BUFFER, gpu_ind)
-		
-	OUT_BUFFER[1] = tuple(np.concatenate((np.asarray(BUFFER1[1][:len(BUFFER1[1])-1]), np.asarray(BUFFER2[1][1])[np.newaxis])))
+	
+	OUT_BUFFER[1] = tuple(np.concatenate((np.asarray(BUFFER1[1][:len(BUFFER1[1])-1]), np.asarray(BUFFER2[1][1])[np.newaxis])))	
+	if additional_args[0] and OUT_BUFFER[1][-1] == 1: # squeeze
+		OUT_BUFFER[1] = OUT_BUFFER[1][:len(OUT_BUFFER[1])-1]
 	return OUT_BUFFER
 	
 from gradient_functions.cosine_sim import *