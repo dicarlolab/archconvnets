e9839450f2b4eaff6285333ec550b83cb74a1ca5
Darren Seibert <darren@mit.edu>
2016-01-30 22:10:42 -0500

    m

--- archconvnets/unsupervised/ntm3/bp_reinforcement.py
+++ archconvnets/unsupervised/ntm3/bp_reinforcement.py
@@ -8,24 +8,24 @@ from architectures.reinforcement import *
 from worlds.panda_world import *
 
 EPS = 1e-1
+EPS_GREED_FINAL_TIME = 4*5000000
 
-save_name = 'reinforcement_%f' % (-EPS)
+save_name = 'reinforcement_%f_EPS_%i_FIN_TIME' % (EPS, EPS_GREED_FINAL_TIME)
 
 free_all_buffers()
 
 ################ init save vars
 N_MOVIES = 6372
 BATCH_SZ = 32
-MEM_SZ = 1000#000
+MEM_SZ = 1000000
 EPS_GREED_FINAL = .1
-EPS_GREED_FINAL_TIME = 4*5000000
 GAMMA = 0.99
 BATCH_SZ = 32
 NETWORK_UPDATE = 10000
 EPOCH_LEN = 11 # length of movie
-SAVE_FREQ = 25*BATCH_SZ #50 #250 # instantaneous checkpoint
+SAVE_FREQ = 1000 #50 #250 # instantaneous checkpoint
 FRAME_LAG = 100 #SAVE_FREQ
-GAME_TRAIN_FRAC = 3 # train on the game X times more than categorization (speed reasons)
+GAME_TRAIN_FRAC = 1#3 # train on the game X times more than categorization (speed reasons)
 
 frame = 0; err = 0;  r_total = 0
 cifar_err = 0; cifar_class = 0
@@ -75,8 +75,11 @@ PARTIALS_PREV = init_partials(LAYERS, MEM_INDS)
 WEIGHTS_F1_INIT = return_buffer(WEIGHTS[find_layer(LAYERS, 'F1')][0])
 
 # set null targets for game action output predictions
+set_buffer(np.zeros((8,1), dtype='single'), WEIGHTS[SYN_CAT_DIFF_IND][1])
 set_buffer(np.zeros((8,1), dtype='single'), WEIGHTS_PREV[SYN_CAT_DIFF_IND][1])
+set_buffer(np.zeros((32,1), dtype='single'), WEIGHTS[SYN_OBJ_DIFF_IND][1])
 set_buffer(np.zeros((32,1), dtype='single'), WEIGHTS_PREV[SYN_OBJ_DIFF_IND][1])
+set_buffer(np.zeros((10,1), dtype='single'), WEIGHTS[CIFAR_DIFF_IND][1])
 set_buffer(np.zeros((10,1), dtype='single'), WEIGHTS_PREV[CIFAR_DIFF_IND][1])
 for action in range(N_C):
 	set_buffer(np.zeros((1,1),dtype='single'), WEIGHTS[GAME_DIFF_IND[action]][1])
@@ -313,15 +316,18 @@ while True:
 		
 		OUTPUT = forward_network(LAYERS, WEIGHTS, OUTPUT, OUTPUT_PREV)
 		
+		err += return_buffer(OUTPUT[GAME_OUT_IND[action_input[trans]]])
+		
 		########### backprop
 		WEIGHT_DERIVS = reverse_network(GAME_OUT_IND[action_input[trans]], LAYERS, WEIGHTS, OUTPUT, OUTPUT_PREV, PARTIALS_PREV, WEIGHT_DERIVS, reset_derivs=False)
-		WEIGHT_DERIVS_OBJ = reverse_network(SYN_OBJ_OUT_IND, LAYERS, WEIGHTS, OUTPUT_MOVIE, OUTPUT_PREV, PARTIALS_PREV, WEIGHT_DERIVS_OBJ, abort_layer='F3_MAX', reset_derivs=False)
-		WEIGHT_DERIVS_CAT = reverse_network(SYN_CAT_OUT_IND, LAYERS, WEIGHTS, OUTPUT_MOVIE, OUTPUT_PREV, PARTIALS_PREV, WEIGHT_DERIVS_CAT, abort_layer='F3_MAX', reset_derivs=False)
-		WEIGHT_DERIVS_CIFAR = reverse_network(CIFAR_OUT_IND, LAYERS, WEIGHTS, OUTPUT_CIFAR, OUTPUT_PREV, PARTIALS_PREV, WEIGHT_DERIVS_CIFAR, abort_layer='F3_MAX', reset_derivs=False)
+		if frame % GAME_TRAIN_FRAC == 0:
+			WEIGHT_DERIVS_OBJ = reverse_network(SYN_OBJ_OUT_IND, LAYERS, WEIGHTS, OUTPUT_MOVIE, OUTPUT_PREV, PARTIALS_PREV, WEIGHT_DERIVS_OBJ, abort_layer='F3_MAX', reset_derivs=False)
+			WEIGHT_DERIVS_CAT = reverse_network(SYN_CAT_OUT_IND, LAYERS, WEIGHTS, OUTPUT_MOVIE, OUTPUT_PREV, PARTIALS_PREV, WEIGHT_DERIVS_CAT, abort_layer='F3_MAX', reset_derivs=False)
+			WEIGHT_DERIVS_CIFAR = reverse_network(CIFAR_OUT_IND, LAYERS, WEIGHTS, OUTPUT_CIFAR, OUTPUT_PREV, PARTIALS_PREV, WEIGHT_DERIVS_CIFAR, abort_layer='F3_MAX', reset_derivs=False)
 		
 		#### update filter weights
 		if(frame - MEM_SZ) % BATCH_SZ == 0 and frame != MEM_SZ:
-			WEIGHT_DERIVS_RMS = update_weights_rms(LAYERS, WEIGHTS, WEIGHT_DERIVS, WEIGHT_DERIVS_RMS, EPS / BATCH_SZ, frame, FRAME_LAG)
+			WEIGHT_DERIVS_RMS = update_weights_rms(LAYERS, WEIGHTS, WEIGHT_DERIVS, WEIGHT_DERIVS_RMS, -EPS / BATCH_SZ, frame, FRAME_LAG)
 			WEIGHT_DERIVS_RMS_OBJ = update_weights_rms(LAYERS, WEIGHTS, WEIGHT_DERIVS_OBJ, WEIGHT_DERIVS_RMS_OBJ, EPS / BATCH_SZ, frame, FRAME_LAG)
 			WEIGHT_DERIVS_RMS_CAT = update_weights_rms(LAYERS, WEIGHTS, WEIGHT_DERIVS_CAT, WEIGHT_DERIVS_RMS_CAT, EPS / BATCH_SZ, frame, FRAME_LAG)
 			WEIGHT_DERIVS_RMS_CIFAR = update_weights_rms(LAYERS, WEIGHTS, WEIGHT_DERIVS_CIFAR, WEIGHT_DERIVS_RMS_CIFAR, EPS / BATCH_SZ, frame, FRAME_LAG)
@@ -342,8 +348,8 @@ while True:
 	# print/save
 	if frame % SAVE_FREQ == 0 and frame != 0:
 		r_log.append([r_total]); r_total = 0
-		corr_log.append([err / SAVE_FREQ])
-		err_log.append([err / SAVE_FREQ]); err = 0
+		corr_log.append(err / SAVE_FREQ)
+		err_log.append(err / SAVE_FREQ); err = 0
 		cifar_err_log.append(cifar_err / SAVE_FREQ); cifar_err = 0;
 		cifar_class_log.append([np.single(cifar_class) / SAVE_FREQ]); cifar_class = 0;
 		cat_err_log.append(cat_err / SAVE_FREQ); cat_err = 0;
@@ -352,8 +358,22 @@ while True:
 		obj_class_log.append([np.single(obj_class) / SAVE_FREQ]); obj_class = 0;
 		
 		print_reinforcement_state(LAYERS, WEIGHTS, WEIGHT_DERIVS_CIFAR, OUTPUT_CIFAR, EPS, CHANCE_RAND, err_log, frame, r_log, cifar_err_log, cifar_class_log, obj_err_log, obj_class_log, cat_err_log, cat_class_log, t_start, save_name, print_names)
-		#save_conv_state(LAYERS, WEIGHTS, WEIGHTS_F1_INIT, save_name, [], [], EPS, err_log, corr_log, cifar_err_log, cifar_class_log, obj_err_log, obj_class_log, cat_err_log, cat_class_log, EPOCH_LEN, N_MOVIES)
 		
+		WEIGHTS_F1 = return_buffer(WEIGHTS[find_layer(LAYERS, 'F1')][0])
+		WEIGHTS_F2 = return_buffer(WEIGHTS[find_layer(LAYERS, 'F2')][0])
+		WEIGHTS_F3 = return_buffer(WEIGHTS[find_layer(LAYERS, 'F3')][0])
+		
+		img = render(x,y, direction, panda, kid, kid_coords, panda_coords, kid_directions, panda_directions)
+		
+		savemat('/home/darren/' + save_name, {'r_total_plot': r_log, 'step': frame, 'img': img, 'err_plot': err_log, \
+			'panda_coords_recent': panda_coords_recent, 'kid_coords_recent': kid_coords_recent, \
+			'panda_directions_recent': panda_directions_recent, 'kid_directions_recent': kid_directions_recent,
+			'x_recent': x_recent, 'y_recent': y_recent, 'direction_recent': direction_recent, 'action_recent': action_recent,
+			'r_recent': r_recent, 'frame': frame,'N_MOVIES': N_MOVIES, \
+			'err_log': err_log, 'corr_log': corr_log, 'cifar_err_log': cifar_err_log, 'cifar_class_log': cifar_class_log, 'EPS': EPS, \
+			'cat_err_log': cat_err_log, 'cat_class_log': cat_class_log, 'obj_err_log': obj_err_log, 'obj_class_log': obj_class_log,\
+			'F1_init': WEIGHTS_F1_INIT, 'F1': WEIGHTS_F1, 'F2': WEIGHTS_F2, 'F3': WEIGHTS_F3, 'EPOCH_LEN': EPOCH_LEN})
+			
 		t_start = time.time()
 		
 	frame += 1
